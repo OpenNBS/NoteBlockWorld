a8e170dc5ef4c2b69bf9c073f0c257b0c59389d7
Bentroen <bemcdc@gmail.com>
2023-09-08 20:44:52 -0300

    fix: show/hide file selection box & song data form on upload page

--- web/src/app/upload/page.tsx
+++ web/src/app/upload/page.tsx
@@ -19,23 +19,182 @@ const Option = styled.option.attrs({
   className: "bg-zinc-900",
 })``;
 
+const SongSelector = ({
+  onFileSelect,
+}: {
+  onFileSelect: (file: File) => void;
+}) => {
+  return (
+    <div
+      className="flex flex-col items-center gap-6 h-fit w-[50vw] border-dashed border-4 border-zinc-700 p-8 mb-4"
+      onDragOver={(e) => e.preventDefault()}
+    >
+      <FontAwesomeIcon icon={faFileAudio} size="5x" className="text-zinc-600" />
+      <div className="text-center">
+        <p className="font-semibold text-xl">Drag and drop your song</p>
+        <p className="text-zinc-400">or select a file below</p>
+      </div>
+      <label
+        htmlFor="uploadNbsFile"
+        className="px-3 py-2 bg-blue-500 hover:bg-blue-400 rounded-lg text-white cursor-pointer"
+      >
+        Select file
+      </label>
+      <input
+        type="file"
+        name="nbsFile"
+        id="uploadNbsFile"
+        accept=".nbs"
+        className="z-[-1] absolute opacity-0"
+        onChange={(e) => {
+          e.preventDefault;
+          onFileSelect(e.target.files?.[0] as File);
+        }}
+        onDrop={(e) => {
+          e.preventDefault();
+          onFileSelect(e.dataTransfer.files?.[0]);
+        }}
+      />
+    </div>
+  );
+};
+
+const UploadForm = () => {
+  return (
+    <form
+      action="http://localhost:5000/upload"
+      method="POST"
+      encType="multipart/form-data"
+    >
+      <div className="flex flex-col h-fit w-1/2 gap-6">
+        {/* Title */}
+        <div>
+          <label htmlFor="name">Title*</label>
+          <Input type="text" name="title" id="title" className="" />
+        </div>
+
+        {/* Description */}
+        <div>
+          <label htmlFor="description">Description</label>
+          <textarea
+            name="description"
+            id="description"
+            className="block h-48 w-full rounded-lg bg-transparent border-2 border-zinc-500 p-2"
+          />
+        </div>
+
+        {/* Tags */}
+        <div>
+          <label htmlFor="tags">Tags</label>
+          <Input type="text" name="tags" id="tags" className="block" />
+          <p className="text-sm text-zinc-500">(Separate tags with a comma)</p>
+        </div>
+
+        {/* Author */}
+        <div className="flex flex-row gap-8 justify-between">
+          <div className="flex-1">
+            <label htmlFor="artist">Author</label>
+            <Input
+              type="text"
+              name="artist"
+              id="artist"
+              value="Bentroen"
+              disabled={true}
+              className=""
+            />
+          </div>
+          <div className="flex-1">
+            <label htmlFor="album">Original author </label>
+            <Input type="text" name="album" id="album" className="block" />
+            <p className="text-sm text-zinc-500">
+              (Leave blank if it's an original song)
+            </p>
+          </div>
+        </div>
+
+        {/* Genre */}
+        <div className="flex flex-row gap-8 justify-between">
+          <div className="flex-1">
+            <label htmlFor="category">Category</label>
+            <Select name="category" id="category">
+              <Option value="volvo">Gaming</Option>
+              <Option value="saab">Movies & TV</Option>
+              <Option value="mercedes">Anime</Option>
+              <Option value="audi">Vocaloid</Option>
+              <Option value="audi">Rock</Option>
+              <Option value="audi">Pop</Option>
+              <Option value="audi">Electronic</Option>
+              <Option value="audi">Ambient</Option>
+              <Option value="audi">Jazz</Option>
+              <Option value="audi">Classical</Option>
+            </Select>
+          </div>
+          <div className="flex-1">
+            <label htmlFor="playlist">Playlist</label>
+            <Select name="playlist" id="playlist">
+              <Option value="playlist1">None</Option>
+              <Option value="playlist1">Playlist 1</Option>
+              <Option value="playlist2">Playlist 2</Option>
+              <Option value="playlist3">Playlist 3</Option>
+            </Select>
+          </div>
+        </div>
+
+        {/* Thumbnail */}
+        <div>
+          <p>Thumbnail</p>
+          <div className="flex flex-col items-center gap-6 h-80 w-full rounded-lg border-2 border-zinc-500 p-8 mb-4"></div>
+        </div>
+
+        {/* Visibility */}
+        <div className="flex flex-row gap-8 justify-between">
+          <div className="flex-1">
+            <label htmlFor="visibility">Visibility</label>
+            <Select name="visibility" id="visibility">
+              <Option value="public">Public</Option>
+              <Option value="public">Unlisted</Option>
+              <Option value="private">Private</Option>
+            </Select>
+          </div>
+          <div className="flex-1">
+            <label htmlFor="license">License</label>
+            <Select name="license" id="license">
+              <Option value="cc">No license</Option>
+              <Option value="cc">Creative Commons CC BY 4.0</Option>
+              <Option value="cc">Public domain</Option>
+            </Select>
+          </div>
+        </div>
+
+        {/* Allow download */}
+        <div className="flex-1">
+          <input
+            type="checkbox"
+            className="accent-blue scale-150 mr-3"
+            name="allowDownload"
+            id="allowDownload"
+          />
+          <label htmlFor="allowDownload">
+            Allow other users to download the NBS file
+          </label>
+        </div>
+
+        <div className="h-4"></div>
+
+        {/* Upload button */}
+        <button
+          type="submit"
+          className="w-32 ml-auto p-3 font-semibold bg-blue-500 hover:bg-blue-400 uppercase rounded-lg"
+        >
+          Upload
+        </button>
+      </div>
+    </form>
+  );
+};
+
 const UploadPage = () => {
   const [file, setFile] = useState<File | null>(null);
-  const [fileName, setFileName] = useState<string | null>(null);
-  const [fileSize, setFileSize] = useState<number | null>(null);
-  const [fileType, setFileType] = useState<string | null>(null);
-  const [fileUrl, setFileUrl] = useState<string | null>(null);
-
-  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      setFile(file);
-      setFileName(file.name);
-      setFileSize(file.size);
-      setFileType(file.type);
-      setFileUrl(URL.createObjectURL(file));
-    }
-  };
 
   const onUpload = async () => {
     if (!file) {
@@ -57,169 +216,8 @@ const UploadPage = () => {
   return (
     <main className="p-8 h-full w-full flex flex-col">
       <h1 className="text-3xl font-semibold">Upload song</h1>
-      <div className="h-10"></div>
-
-      <form
-        action="http://localhost:5000/upload"
-        method="POST"
-        encType="multipart/form-data"
-      >
-        <div className="flex flex-col h-fit w-1/2 gap-6">
-          {/* File upload */}
-          <div className="m-2">
-            <div
-              className="flex flex-col items-center gap-6 h-fit w-full border-dashed border-4 border-zinc-700 p-8 mb-4"
-              onDragOver={(e) => e.preventDefault()}
-            >
-              <FontAwesomeIcon
-                icon={faFileAudio}
-                size="5x"
-                className="text-zinc-600"
-              />
-              <div className="text-center">
-                <p className="font-semibold text-xl">Drag and drop your song</p>
-                <p className="text-zinc-400">or select a file below</p>
-              </div>
-              <label
-                htmlFor="uploadNbsFile"
-                className="px-3 py-2 bg-blue-500 hover:bg-blue-400 rounded-lg text-white cursor-pointer"
-              >
-                Select file
-              </label>
-              <input
-                type="file"
-                name="nbsFile"
-                id="uploadNbsFile"
-                className="z-[-1] absolute opacity-0"
-              />
-            </div>
-          </div>
-
-          {/* Title */}
-          <div>
-            <label htmlFor="name">Title*</label>
-            <Input type="text" name="title" id="title" className="" />
-          </div>
-
-          {/* Description */}
-          <div>
-            <label htmlFor="description">Description</label>
-            <textarea
-              name="description"
-              id="description"
-              className="block h-48 w-full rounded-lg bg-transparent border-2 border-zinc-500 p-2"
-            />
-          </div>
-
-          {/* Tags */}
-          <div>
-            <label htmlFor="tags">Tags</label>
-            <Input type="text" name="tags" id="tags" className="block" />
-            <p className="text-sm text-zinc-500">
-              (Separate tags with a comma)
-            </p>
-          </div>
-
-          {/* Author */}
-          <div className="flex flex-row gap-8 justify-between">
-            <div className="flex-1">
-              <label htmlFor="artist">Author</label>
-              <Input
-                type="text"
-                name="artist"
-                id="artist"
-                value="Bentroen"
-                disabled={true}
-                className=""
-              />
-            </div>
-            <div className="flex-1">
-              <label htmlFor="album">Original author </label>
-              <Input type="text" name="album" id="album" className="block" />
-              <p className="text-sm text-zinc-500">
-                (Leave blank if it's an original song)
-              </p>
-            </div>
-          </div>
-
-          {/* Genre */}
-          <div className="flex flex-row gap-8 justify-between">
-            <div className="flex-1">
-              <label htmlFor="category">Category</label>
-              <Select name="category" id="category">
-                <Option value="volvo">Gaming</Option>
-                <Option value="saab">Movies & TV</Option>
-                <Option value="mercedes">Anime</Option>
-                <Option value="audi">Vocaloid</Option>
-                <Option value="audi">Rock</Option>
-                <Option value="audi">Pop</Option>
-                <Option value="audi">Electronic</Option>
-                <Option value="audi">Ambient</Option>
-                <Option value="audi">Jazz</Option>
-                <Option value="audi">Classical</Option>
-              </Select>
-            </div>
-            <div className="flex-1">
-              <label htmlFor="playlist">Playlist</label>
-              <Select name="playlist" id="playlist">
-                <Option value="playlist1">None</Option>
-                <Option value="playlist1">Playlist 1</Option>
-                <Option value="playlist2">Playlist 2</Option>
-                <Option value="playlist3">Playlist 3</Option>
-              </Select>
-            </div>
-          </div>
-
-          {/* Thumbnail */}
-          <div>
-            <p>Thumbnail</p>
-            <div className="flex flex-col items-center gap-6 h-80 w-full rounded-lg border-2 border-zinc-500 p-8 mb-4"></div>
-          </div>
-
-          {/* Visibility */}
-          <div className="flex flex-row gap-8 justify-between">
-            <div className="flex-1">
-              <label htmlFor="visibility">Visibility</label>
-              <Select name="visibility" id="visibility">
-                <Option value="public">Public</Option>
-                <Option value="public">Unlisted</Option>
-                <Option value="private">Private</Option>
-              </Select>
-            </div>
-            <div className="flex-1">
-              <label htmlFor="license">License</label>
-              <Select name="license" id="license">
-                <Option value="cc">No license</Option>
-                <Option value="cc">Creative Commons CC BY 4.0</Option>
-                <Option value="cc">Public domain</Option>
-              </Select>
-            </div>
-          </div>
-
-          {/* Allow download */}
-          <div className="flex-1">
-            <input
-              type="checkbox"
-              className="accent-blue scale-150 mr-3"
-              name="allowDownload"
-              id="allowDownload"
-            />
-            <label htmlFor="allowDownload">
-              Allow other users to download the NBS file
-            </label>
-          </div>
-
-          <div className="h-4"></div>
-
-          {/* Upload button */}
-          <button
-            type="submit"
-            className="w-32 ml-auto p-3 font-semibold bg-blue-500 hover:bg-blue-400 uppercase rounded-lg"
-          >
-            Upload
-          </button>
-        </div>
-      </form>
+      <div className="h-10" />
+      {!file ? <SongSelector onFileSelect={setFile} /> : <UploadForm />}
     </main>
   );
 };
