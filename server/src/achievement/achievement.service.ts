14912e044241dd43030d4bcdad233ec77a28cefa
tomast1337 <vycasnicolas@gmail.com>
2024-01-19 19:02:49 -0300

    Add achievement service methods

--- server/src/achievement/achievement.service.ts
+++ server/src/achievement/achievement.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AchievementService {}
+export class AchievementService {
+  public unlockAchievement() {
+    throw new Error('Not implemented');
+  }
+
+  public getAchievements() {
+    throw new Error('Not implemented');
+  }
+
+  public getAchievement() {
+    throw new Error('Not implemented');
+  }
+
+  public createAchievement() {
+    throw new Error('Not implemented');
+  }
+
+  public updateAchievement() {
+    throw new Error('Not implemented');
+  }
+
+  public deleteAchievement() {
+    throw new Error('Not implemented');
+  }
+
+  public getAchievementFromUser() {
+    throw new Error('Not implemented');
+  }
+}
