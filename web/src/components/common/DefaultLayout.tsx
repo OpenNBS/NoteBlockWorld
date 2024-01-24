982771bc3e422a68bdff7cdc390c3a44d945cb34
tomast1337 <vycasnicolas@gmail.com>
2024-01-24 18:42:43 -0300

    Fixed Side bar flicker on page load

--- web/src/components/common/DefaultLayout.tsx
+++ web/src/components/common/DefaultLayout.tsx
@@ -1,13 +1,14 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import Sidebar from "./Sidebar";
+import React, { useCallback, useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import Sidebar from './Sidebar';
 
 type TDefaultLayoutProps = {
   children: React.ReactNode;
   isSidebarOpen: boolean;
+  // eslint-disable-next-line no-unused-vars
   setSidebarOpen: (isOpen: boolean) => void;
 };
 
@@ -18,41 +19,24 @@ const DefaultLayout = ({
 }: TDefaultLayoutProps) => {
   const [showOverlay, setShowOverlay] = useState(true);
 
-  // If the viewport is smaller than 768px, the sidebar is hidden by default
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 768) {
-        setSidebarOpen(false);
-        setShowOverlay(false); // Hide the overlay immediately
-      } else {
+  const setSidebarState = useCallback(
+    (open: boolean) => {
+      if (open) {
         setSidebarOpen(true);
+        setShowOverlay(true);
+      } else {
+        setSidebarOpen(false);
+        setTimeout(() => {
+          setShowOverlay(false);
+        }, 250);
       }
-    };
-    window.addEventListener("resize", handleResize);
+    },
+    [setSidebarOpen]
+  );
 
-    // Do a verification on mount
-    handleResize();
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const setSidebarState = (open: boolean) => {
-    if (open) {
-      setSidebarOpen(true);
-      setShowOverlay(true);
-    } else {
-      setSidebarOpen(false);
-      setTimeout(() => {
-        setShowOverlay(false);
-      }, 250);
-    }
-  };
-
-  const toggleSidebar = () => {
+  const toggleSidebar = useCallback(() => {
     setSidebarState(!isSidebarOpen);
-  };
+  }, [isSidebarOpen, setSidebarState]);
 
   return (
     <>
@@ -62,33 +46,33 @@ const DefaultLayout = ({
           className={`w-64 h-full
                     m-0 z-20 bg-zinc-900
                     shadow-xl shadow-black md:shadow-none md:border-r md:border-zinc-800 rounded
-                    fixed ${isSidebarOpen ? "left-0" : "-left-64"}
+                    fixed ${isSidebarOpen ? 'left-0' : '-left-64'}
                     transition-all duration-[250ms]`}
         >
-          <div className="h-full flex flex-col overflow-y-auto">
+          <div className='h-full flex flex-col overflow-y-auto'>
             <Sidebar toggle={toggleSidebar} />
           </div>
         </div>
 
         {/* Navbar */}
-        <div className="fixed w-full flex flex-row justify-between items-center bg-zinc-900 border-b border-zinc-800 p-2 z-10">
+        <div className='fixed w-full flex flex-row justify-between items-center bg-zinc-900 border-b border-zinc-800 p-2 z-10'>
           {/* Burger button */}
-          <div className="block">
+          <div className='block'>
             <button
-              className="flex items-center px-3 py-2 rounded"
+              className='flex items-center px-3 py-2 rounded'
               onClick={toggleSidebar}
             >
-              <FontAwesomeIcon icon={faBars} size="2x" />
+              <FontAwesomeIcon icon={faBars} size='2x' />
             </button>
           </div>
 
           {/* Logo */}
-          <div className="w-full block h-14">
+          <div className='w-full block h-14'>
             <figure>
               <img
-                src="/nbw-white.png"
-                alt="NoteBlockWorld logo"
-                className="h-10 mx-auto my-2"
+                src='/nbw-white.png'
+                alt='NoteBlockWorld logo'
+                className='h-10 mx-auto my-2'
               />
             </figure>
           </div>
@@ -96,7 +80,7 @@ const DefaultLayout = ({
 
         <div
           className={`h-full ${
-            isSidebarOpen ? "md:ml-64" : "ml-0"
+            isSidebarOpen ? 'md:ml-64' : 'ml-0'
           } bg-zinc-900 pt-24 px-6 sm:px-10 transition-all duration-[250ms]`}
         >
           {children}
@@ -106,7 +90,7 @@ const DefaultLayout = ({
         {showOverlay && (
           <div
             className={`${
-              isSidebarOpen ? "opacity-50" : "opacity-0"
+              isSidebarOpen ? 'opacity-50' : 'opacity-0'
             } md:hidden w-full h-full fixed z-10 top-0 left-0 right-0 bg-black transition-opacity duration-[250ms]`}
             onClick={toggleSidebar}
           ></div>
--- web/src/components/common/Sidebar.tsx
+++ web/src/components/common/Sidebar.tsx
@@ -1,19 +1,19 @@
-import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faMusic } from "@fortawesome/free-solid-svg-icons";
-import Link from "next/link";
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faMusic } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
 
 const SplitHR = styled.hr.attrs({
-  className: "border-zinc-200 dark:border-zinc-700 my-4",
+  className: 'border-zinc-200 dark:border-zinc-700 my-4',
 })``;
 
 const MenuList = styled.ul.attrs({
-  className: "",
+  className: '',
 })``;
 
 const MenuItem = styled.li.attrs({
   className:
-    "flex items-center p-2 rounded-lg text-white hover:bg-zinc-800 cursor-pointer",
+    'flex items-center p-2 rounded-lg text-white hover:bg-zinc-800 cursor-pointer',
 })``;
 
 type TSidebarProps = {
@@ -23,32 +23,32 @@ type TSidebarProps = {
 const Sidebar = ({ toggle }: TSidebarProps) => {
   return (
     <>
-      <div className="flex flex-row justify-between items-center p-2 mr-3">
-        <div className="col col-span-1">
+      <div className='flex flex-row justify-between items-center p-2 mr-3'>
+        <div className='col col-span-1'>
           <button
-            className="flex items-center px-3 py-2 rounded text-white"
+            className='flex items-center px-3 py-2 rounded text-white'
             onClick={toggle}
           >
-            <FontAwesomeIcon icon={faBars} size="2x" />
+            <FontAwesomeIcon icon={faBars} size='2x' />
           </button>
         </div>
-        <div className="col col-span-2">
+        <div className='col col-span-2'>
           <figure>
             <img
-              src="/nbw-white.png"
-              alt="NoteBlockWorld logo"
-              className="h-10 mx-auto my-2"
+              src='/nbw-white.png'
+              alt='NoteBlockWorld logo'
+              className='h-10 mx-auto my-2'
             />
           </figure>
         </div>
       </div>
-      <hr className="border-zinc-200 dark:border-zinc-800" />
-      <div className="flex flex-col h-full justify-between p-3">
+      <hr className='border-zinc-200 dark:border-zinc-800' />
+      <div className='flex flex-col h-full justify-between p-3'>
         <div>
           <MenuList>
-            <Link href="/dashboard">
+            <Link href='/dashboard'>
               <MenuItem>
-                <FontAwesomeIcon icon={faMusic} size="lg" className="pr-3" />
+                <FontAwesomeIcon icon={faMusic} size='lg' className='pr-3' />
                 Open Note Block Studio
               </MenuItem>
             </Link>
@@ -59,24 +59,24 @@ const Sidebar = ({ toggle }: TSidebarProps) => {
 
           {/* User info */}
           {true && ( // TODO: replace with "session"
-            <div className="space-y-2">
-              <div className="flex flex-row gap-2 items-center p-2 text-base text-white font-normal rounded-lg">
+            <div className='space-y-2'>
+              <div className='flex flex-row gap-2 items-center p-2 text-base text-white font-normal rounded-lg'>
                 <picture>
                   <img
-                    className="flex-none w-[2.5rem] min-w-[2.5rem] rounded-full"
-                    src="/vercel.json"
-                    alt=""
+                    className='flex-none w-[2.5rem] min-w-[2.5rem] rounded-full'
+                    src='/vercel.json'
+                    alt=''
                   />
                 </picture>
-                <div className="leading-tight">
-                  <span className="font-semibold">Note Block World</span>
+                <div className='leading-tight'>
+                  <span className='font-semibold'>Note Block World</span>
 
-                  <p className="text-xs text-zinc-400">
+                  <p className='text-xs text-zinc-400'>
                     opennbs@example.com
                     <br />(
                     <a
-                      onClick={() => console.log("Signing out")}
-                      className="underline text-zinc-400 hover:text-zinc-300 hover:cursor-pointer"
+                      onClick={() => console.log('Signing out')}
+                      className='underline text-zinc-400 hover:text-zinc-300 hover:cursor-pointer'
                     >
                       sign out
                     </a>
@@ -87,12 +87,12 @@ const Sidebar = ({ toggle }: TSidebarProps) => {
             </div>
           )}
           <SplitHR />
-          <div className="text-xs text-center text-zinc-500">
+          <div className='text-xs text-center text-zinc-500'>
             <p>
-              © 2023 OpenNBS |{" "}
+              © 2023 OpenNBS |{' '}
               <Link
-                href="/privacy"
-                className="text-xs text-zinc-500 hover:text-zinc-400 underline hover:cursor-pointer"
+                href='/privacy'
+                className='text-xs text-zinc-500 hover:text-zinc-400 underline hover:cursor-pointer'
               >
                 Privacy
               </Link>
