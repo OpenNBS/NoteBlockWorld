119d2d55a4f25f27aa0830818de72c660cb430f9
tomast1337 <vycasnicolas@gmail.com>
2024-01-24 18:41:02 -0300

    Add getUser and getUserPaginated endpoints to UserController

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+  Req,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
+import { UserService } from './user.service';
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(
+    @Inject(UserService)
+    private readonly userService: UserService,
+  ) {}
+
+  @Get()
+  @ApiTags('user')
+  @ApiBearerAuth()
+  async getUser(@Query() query: any) {
+    return await this.userService.getUserByEmailOrId(query);
+  }
+
+  @Get()
+  @ApiTags('user')
+  @ApiBearerAuth()
+  async getUserPaginated(@Query() query: any) {
+    return await this.userService.getUserPaginated(query);
+  }
+}
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './entity/user.entity';
 import { CreateUser } from './dto/CreateUser.dto';
 import { validate } from 'class-validator';
+import { PageQuery } from '@server/common/dto/PageQuery.dto';
+import { GetUser } from './dto/GetUser.dto';
 
 @Injectable()
 export class UserService {
@@ -20,5 +22,38 @@ export class UserService {
   public async findByEmail(email: string): Promise<UserDocument | null> {
     const user = await this.userModel.findOne({ email }).exec();
     return user;
+  }
+
+  public async findByID(objectID: string): Promise<UserDocument | null> {
+    const user = await this.userModel.findById(objectID).exec();
+    return user;
+  }
+
+  public getUserPaginated(query: PageQuery) {
+    const { page, limit } = query;
+    const options = {
+      page: parseInt(page, 10) || 1,
+      limit: parseInt(limit, 10) || 10,
+    };
+    return this.userModel.find({});
+  }
+  public async getUserByEmailOrId(query: GetUser) {
+    const { email, id, username } = query;
+    if (email) {
+      return await this.findByEmail(email);
+    }
+    if (id) {
+      return await this.findByID(id);
+    }
+    if (username) {
+      throw new HttpException(
+        'Username is not supported yet',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    throw new HttpException(
+      'You must provide an email or an id',
+      HttpStatus.BAD_REQUEST,
+    );
   }
 }
