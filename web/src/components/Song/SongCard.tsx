d975f86e0935595560030feff9d5110383677cb0
Bentroen <bemcdc@gmail.com>
2024-01-25 21:46:01 -0300

    refactor: remove indirection on `SongCard` component structure

--- web/src/components/Song/SongCard.tsx
+++ web/src/components/Song/SongCard.tsx
@@ -1,40 +1,30 @@
 import { SongType } from '@web/src/types/Song';
 import styled from 'styled-components';
 
-const SongCardContainer = styled.div.attrs({
-  className:
-    'flex flex-col gap-2 bg-zinc-800  hover:bg-zinc-700 rounded-lg hover:scale-105 cursor-pointer w-fit h-60 transition-all duration-200',
-})``;
-
-const SongCardImage = styled.div.attrs({
-  className: 'h-40 w-full object-cover rounded-lg',
-})``;
-
-const SongCardInfo = styled.div.attrs({
-  className: 'flex flex-col gap-1 px-4',
-})``;
-
-const SongCardTitle = styled.h3.attrs({
-  className: 'text-lg font-semibold',
-})``;
 type SongCardProps = {
   song: SongType;
 };
+
 const SongCard = ({ song }: Partial<SongCardProps>) => {
   return (
-    <SongCardContainer>
-      <SongCardImage>
+    <div className='flex flex-col gap-2 bg-zinc-800  hover:bg-zinc-700 rounded-lg hover:scale-105 cursor-pointer w-fit h-60 transition-all duration-200'>
+      {/* Song image */}
+      <div className='h-40 w-full object-cover rounded-lg'>
         <img
           src='/demo.png'
           alt='Song cover'
           className='w-full h-full object-cover rounded-lg'
         />
-      </SongCardImage>
-      <SongCardInfo>
-        <SongCardTitle>Song title</SongCardTitle>
+      </div>
+
+      {/* Song info */}
+      <div className='flex flex-col gap-1 px-4'>
+        {/* Song title */}
+        <h3 className='text-lg font-semibold'>Song title</h3>
+        {/* Song author */}
         <p className='text-sm'>by author</p>
-      </SongCardInfo>
-    </SongCardContainer>
+      </div>
+    </div>
   );
 };
 
