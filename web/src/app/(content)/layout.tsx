c9f565ebc3a1b1138e2ed3e1947b15488b17fec7
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-01-27 03:33:46 -0300

    refactor: remove unnecessary nested layouts

--- web/src/app/(content)/layout.tsx
+++ web/src/app/(content)/layout.tsx
@@ -1,17 +1,10 @@
-import { useState } from 'react';
 import '@web/src/app/globals.css';
 import NavbarLayout from '@web/src/components/Layout/NavbarLayout';
 
-export default function RootLayout({
+export default function ContentLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang='en' className='h-full'>
-      <body className={'h-full'}>
-        <NavbarLayout>{children}</NavbarLayout>
-      </body>
-    </html>
-  );
+  return <NavbarLayout>{children}</NavbarLayout>;
 }
