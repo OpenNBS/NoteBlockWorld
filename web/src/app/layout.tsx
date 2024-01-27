1db9fc2ee68fda5919d4db33a9b54d2c9a213219
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-01-27 03:33:23 -0300

    fix: fix body of page not occupying full height

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en' className='h-full'>
-      <body className={lato.className + ' h-full'}>{children}</body>
+    <html lang='en' className='min-h-screen'>
+      <body className={lato.className + ' min-h-screen'}>{children}</body>
     </html>
   );
 }
--- web/src/components/Layout/NavbarLayout.tsx
+++ web/src/components/Layout/NavbarLayout.tsx
@@ -8,51 +8,57 @@ type TNavbarLayoutProps = {
 const NavbarLayout = ({ children }: TNavbarLayoutProps) => {
   return (
     <>
-      <div className={`w-full h-full`}>
+      <div className={`w-full min-h-screen flex flex-col`}>
         {/* Navbar */}
-        <div className='fixed w-full h-14 flex flex-row justify-between items-center bg-zinc-900 border-b border-zinc-700 p-2 z-10'>
-          {/* Logo */}
-          <div className='flex-grow'>
-            <figure>
-              <img
-                src='/nbw-white.png'
-                alt='NoteBlockWorld logo'
-                className='h-10 mx-auto my-2'
-              />
-            </figure>
-          </div>
+        <div className='flex-shrink'>
+          <div className='fixed w-full h-14 flex flex-row justify-between items-center bg-zinc-900 border-b border-zinc-700 p-2 z-10'>
+            {/* Logo */}
+            <div className='flex-grow'>
+              <figure>
+                <img
+                  src='/nbw-white.png'
+                  alt='NoteBlockWorld logo'
+                  className='h-10 mx-auto my-2'
+                />
+              </figure>
+            </div>
 
-          {/* Sign in / Profile */}
-          <div className='flex-shrink mr-10'>
-            <a href='/login'>
-              <div className='flex justify-between items-center gap-2 px-1 border border-blue-500 h-8 rounded-full'>
-                <div className='h-6'>
-                  <FontAwesomeIcon
-                    icon={faUserCircle}
-                    className='h-full text-blue-500'
-                  />
+            {/* Sign in / Profile */}
+            <div className='flex-shrink mr-10'>
+              <a href='/login'>
+                <div className='flex justify-between items-center gap-2 px-1 border border-blue-500 h-8 rounded-full'>
+                  <div className='h-6'>
+                    <FontAwesomeIcon
+                      icon={faUserCircle}
+                      className='h-full text-blue-500'
+                    />
+                  </div>
+                  <span className='text-sm mr-2 text-blue-400 font-semibold'>
+                    Sign in
+                  </span>
                 </div>
-                <span className='text-sm mr-2 text-blue-400 font-semibold'>
-                  Sign in
-                </span>
-              </div>
-            </a>
+              </a>
+            </div>
           </div>
         </div>
 
         {/* Page content */}
-        <div className='bg-zinc-900 pt-24 px-6 sm:px-10 pb-10'>{children}</div>
+        <div className='flex-grow bg-zinc-900 pt-24 px-6 sm:px-10 pb-10'>
+          {children}
+        </div>
 
         {/* Footer */}
-        <div className='w-full h-12 flex flex-row justify-center items-center bg-zinc-900 border-t border-zinc-700 p-2 z-10'>
-          <span className='text-sm text-zinc-500'>
-            <p>
-              © 2024{' '}
-              <a href='https://opennbs.org/' className='underline'>
-                OpenNBS
-              </a>
-            </p>
-          </span>
+        <div className='flex-shrink'>
+          <div className='w-full h-12 flex flex-row justify-center items-center bg-zinc-900 border-t border-zinc-700 p-2 z-10'>
+            <span className='text-sm text-zinc-500'>
+              <p>
+                © 2024{' '}
+                <a href='https://opennbs.org/' className='underline'>
+                  OpenNBS
+                </a>
+              </p>
+            </span>
+          </div>
         </div>
       </div>
     </>
