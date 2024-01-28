3e072f7a93a5e1fbcc4dedfb006f08911fa325f2
tomast1337 <vycasnicolas@gmail.com>
2024-01-28 14:08:35 -0300

    Tailwind fix

--- web/src/app/(content)/layout.tsx
+++ web/src/app/(content)/layout.tsx
@@ -1,11 +1,10 @@
 import '@web/src/app/globals.css';
-
-import NavbarLayout from '@web/src/server/components/Layout/NavbarLayout';
+import Layout from '@web/src/server/components/Layout/Layout';
 
 export default async function ContentLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <NavbarLayout>{children}</NavbarLayout>;
+  return <Layout>{children}</Layout>;
 }
