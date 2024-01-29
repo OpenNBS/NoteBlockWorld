8a576a31dbf483c4c91ac9b8cbcd82e7b0881a0e
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-01-29 01:04:38 -0300

    refactor: move all styles of login page to `LoginPage` component

--- web/src/app/(auth)/login/page.tsx
+++ web/src/app/(auth)/login/page.tsx
@@ -5,11 +5,7 @@ import { LoginPage } from './loginPage';
 const Login = async () => {
   const isLogged = await checkLogin();
   if (isLogged) redirect('/browse');
-  return (
-    <main className='w-screen h-screen p-6 text-center text-balance bg-zinc-900 flex items-center justify-center'>
-      <LoginPage />
-    </main>
-  );
+  return <LoginPage />;
 };
 
 export default Login;
