c8809ed1638f9911ab2709de82969b44421b2a6a
tomast1337 <vycasnicolas@gmail.com>
2024-01-28 23:10:21 -0300

    Add new pages and server utilities for user and song data retrieval

---
+++ web/src/app/(user)/my-songs/page.tsx
@@ -0,0 +1,22 @@
+import Layout from '@web/src/server/components/Layout/Layout';
+import { getUserSongs } from '@web/src/server/util/Song';
+import { checkLogin, getUserData } from '@web/src/server/util/utils';
+
+const MySongsPage = async () => {
+  const isLogged = await checkLogin();
+  let userData = undefined;
+  let userSongs = undefined;
+  if (isLogged) {
+    userData = await getUserData();
+    userSongs = await getUserSongs(userData.id);
+  }
+  return (
+    <main className='w-full h-full m-auto text-center bg-zinc-900 flex items-center justify-center'>
+      <Layout>
+        <h1>My Songs</h1>
+      </Layout>
+    </main>
+  );
+};
+
+export default MySongsPage;
