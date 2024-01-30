f32671a32f174dcb47dc49f44cd08b4a15d26cb2
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-01-29 22:34:43 -0300

    fix: replace grid breakpoints with grid-auto-fit utility

--- web/src/client/components/browse/SongCardGroup.tsx
+++ web/src/client/components/browse/SongCardGroup.tsx
@@ -1,6 +1,6 @@
 const SongCardGroup = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full items-center gap-4'>
+    <div className='grid grid-auto-fit-md w-full items-center gap-4'>
       {children}
     </div>
   );
