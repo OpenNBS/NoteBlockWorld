c118f1a93c30956d1d9104471ff01777ef4b9d60
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-01-31 01:56:54 -0300

    feat: add picked file name display and allow changing file

--- web/src/client/components/upload/UploadSong.tsx
+++ web/src/client/components/upload/UploadSong.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { faFileAudio } from '@fortawesome/free-solid-svg-icons';
+import { faFile, faFileAudio } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useCallback } from 'react';
 import { styled } from 'styled-components';
@@ -249,8 +249,33 @@ const UploadForm = () => {
 };
 
 const UploadSong = () => {
-  const { song } = useUploadSongProvider();
-  return <>{!song ? <SongSelector /> : <UploadForm />}</>;
+  const { song, filename, setFile } = useUploadSongProvider();
+  return (
+    <>
+      <div className='flex flex-row flex-wrap justify-between items-center gap-8'>
+        <h1 className='text-3xl font-semibold'>Upload song</h1>
+
+        {song && (
+          <div className='flex flex-row gap-4 items-center text-zinc-500'>
+            <div className='flex flex-row gap-2 items-center'>
+              <FontAwesomeIcon icon={faFile} size='lg' />
+              <p className='text-md'>{filename}</p>
+            </div>
+            <button
+              className='px-3 py-2 bg-blue-500 hover:bg-blue-400 rounded-lg text-white'
+              onClick={() => {
+                setFile(null);
+              }}
+            >
+              Change file
+            </button>
+          </div>
+        )}
+      </div>
+      <div className='h-10' />
+      {!song ? <SongSelector /> : <UploadForm />}
+    </>
+  );
 };
 
 export const UploadSongPage = () => {
