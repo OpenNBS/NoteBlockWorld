8b4a40d77a617563e0d0150a10b80d70edb0ff4e
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-02-01 19:46:33 -0300

    chore: remove imported layout component

--- web/src/app/(content)/(user)/my-songs/page.tsx
+++ web/src/app/(content)/(user)/my-songs/page.tsx
@@ -1,4 +1,3 @@
-import Layout from '@web/src/server/components/Layout/NavbarLayout';
 import { getUserSongs } from '@web/src/server/util/Song';
 import { checkLogin, getUserData } from '@web/src/server/util/utils';
 
