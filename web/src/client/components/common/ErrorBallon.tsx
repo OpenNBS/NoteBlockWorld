41139ec82ac292e5c66aa6939342a381533d684d
tomast1337 <vycasnicolas@gmail.com>
2024-01-31 22:16:40 -0300

    Added ErrorBallon component Co-authored-by: Bernardo Costa <Bentroen@users.noreply.github.com>

---
+++ web/src/client/components/common/ErrorBallon.tsx
@@ -0,0 +1,21 @@
+import { GlobalError } from 'react-hook-form';
+
+type ErrorBallonProps = GlobalError & { isVisible: boolean };
+
+export const ErrorBallon = ({ message, isVisible }: ErrorBallonProps) => {
+  return (
+    <>
+      <div
+        className={`bg-red-600 p-2 absolute rounded-md mt-2 z-10 ${
+          isVisible ? 'block' : 'hidden'
+        }`}
+      >
+        {message}
+      </div>
+      <div
+        // spacing
+        className='block h-8'
+      />
+    </>
+  );
+};
