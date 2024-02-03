2855cd728b79c45496e5a113386d6925c69484ff
tomast1337 <vycasnicolas@gmail.com>
2024-02-03 17:08:30 -0300

    added a temporary avatar

---
+++ web/src/modules/shared/components/client/UserMenuButton.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { LoggedUserData } from '@web/src/modules/auth/types/User';
+import { RandomAvatar } from 'react-random-avatars';
+export function UserMenuButton({ userData }: { userData: LoggedUserData }) {
+  return (
+    <>
+      <button className='h-8 w-8 relative'>
+        <RandomAvatar mode={'pattern'} name={userData.username} size={30} />
+        {/*
+        <img
+        src='/bentroen.png'
+        className='absolute top-0 left-0 h-full w-full rounded-full'
+        />
+        */}
+        <div className='absolute top-0 left-0 h-full w-full bg-black rounded-full opacity-0 hover:opacity-30 transition-opacity duration-150'></div>
+      </button>
+    </>
+  );
+}
--- web/src/modules/shared/components/layout/UserMenu.tsx
+++ web/src/modules/shared/components/layout/UserMenu.tsx
@@ -1,21 +1,6 @@
 import { LoggedUserData } from '@web/src/modules/auth/types/User';
+import { UserMenuButton } from '../client/UserMenuButton';
 import { Popover, PopoverContent, PopoverTrigger } from './popover';
-import { PopoverArrow } from '@radix-ui/react-popover';
-
-function UserMenuButton({ userData }: { userData: LoggedUserData }) {
-  return (
-    <>
-      <button className='h-8 w-8 relative'>
-        <img
-          src='/bentroen.png'
-          className='absolute top-0 left-0 h-full w-full rounded-full'
-        />
-        <div className='absolute top-0 left-0 h-full w-full bg-black rounded-full opacity-0 hover:opacity-30 transition-opacity duration-150'></div>
-      </button>
-    </>
-  );
-}
-
 export function UserMenu({ userData }: { userData: LoggedUserData }) {
   return (
     <Popover>
