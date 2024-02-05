1375433c015fabc205f12a79292fa4c2122cdf9a
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-02-05 04:54:49 -0300

    fix: prevent overflow on too long uploaded song filename

--- web/src/modules/upload/components/client/UploadSong.tsx
+++ web/src/modules/upload/components/client/UploadSong.tsx
@@ -13,18 +13,18 @@ const UploadSong = () => {
   const { song, filename } = useUploadSongProvider();
   return (
     <>
-      <div className='flex flex-row flex-wrap justify-between items-center gap-8'>
-        <div>
-          <h1 className='text-3xl font-semibold'>Upload song</h1>
+      <div className='flex flex-row justify-between items-center gap-12'>
+        <div className='flex-1'>
+          <h1 className='text-3xl font-semibold text-nowrap'>Upload song</h1>
         </div>
         {song && (
-          <div className='flex flex-row gap-4 items-center text-zinc-500'>
-            <div className='flex flex-row gap-2 items-center'>
+          <div className='flex-shrink min-w-0 flex flex-row gap-4 items-center text-zinc-500'>
+            <div className='flex-shrink min-w-0 max-w-96 flex flex-row gap-2 items-center'>
               <FontAwesomeIcon icon={faFile} size='lg' />
-              <p className='text-md'>{filename}</p>
+              <p className='text-md flex-shrink min-w-0 truncate'>{filename}</p>
             </div>
             <button
-              className='px-3 py-2 bg-blue-500 hover:bg-blue-400 rounded-lg text-white'
+              className='text-nowrap px-3 py-2 bg-blue-500 hover:bg-blue-400 rounded-lg text-white'
               onClick={() => {
                 window.location.reload();
               }}
