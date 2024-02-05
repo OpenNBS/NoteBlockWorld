6c0a8163b8facdd41f92e846b0b0c628c87fb361
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-02-05 06:49:06 -0300

    fix: fix horizontal padding on footer

--- web/src/modules/shared/components/layout/Footer.tsx
+++ web/src/modules/shared/components/layout/Footer.tsx
@@ -27,35 +27,37 @@ const FooterIcon = ({ icon, href }: { href: string; icon: IconDefinition }) => (
 
 export function Footer() {
   return (
-    <footer className='w-full h-14 flex flex-row justify-between items-center border-t border-zinc-700 p-2 z-10 text-sm text-zinc-400'>
-      {/* Social links */}
-      <div className='flex-grow flex flex-row gap-2'>
-        <FooterIcon icon={faTwitter} href='https://twitter.com/OpenNBS' />
-        <FooterIcon icon={faYoutube} href='https://youtube.com/@OpenNBS' />
-        <FooterIcon icon={faGithub} href='https://github.com/OpenNBS' />
-        <FooterIcon icon={faDiscord} href='https://discord.gg' />
-        {/* <FooterIcon icon={faEarth} href='https://discord.gg' /> */}
-        {/* <FooterIcon icon={faEnvelope} href='mailto:opennbs@gmail.com' /> */}
-        {/* <FooterIcon icon={faHeart} href='https://opencollective.com/OpenNBS' /> */}
-      </div>
+    <footer className='flex items-center justify-center w-full h-14 px-6 sm:px-10 py-2 z-10 border-t border-zinc-700 text-sm text-zinc-400'>
+      <div className='flex-1 flex flex-row justify-around items-center max-w-screen-xl'>
+        {/* Social links */}
+        <div className='flex-grow flex flex-row gap-2'>
+          <FooterIcon icon={faTwitter} href='https://twitter.com/OpenNBS' />
+          <FooterIcon icon={faYoutube} href='https://youtube.com/@OpenNBS' />
+          <FooterIcon icon={faGithub} href='https://github.com/OpenNBS' />
+          <FooterIcon icon={faDiscord} href='https://discord.gg' />
+          {/* <FooterIcon icon={faEarth} href='https://discord.gg' /> */}
+          {/* <FooterIcon icon={faEnvelope} href='mailto:opennbs@gmail.com' /> */}
+          {/* <FooterIcon icon={faHeart} href='https://opencollective.com/OpenNBS' /> */}
+        </div>
 
-      <div className=''>
-        <p>
-          © {new Date().getFullYear()}{' '}
-          <Link href='https://opennbs.org/' className='hover:underline'>
-            OpenNBS
+        <div className='flex-shrink'>
+          <p>
+            © {new Date().getFullYear()}{' '}
+            <Link href='https://opennbs.org/' className='hover:underline'>
+              OpenNBS
+            </Link>
+          </p>
+        </div>
+
+        <div className='flex-grow flex justify-end'>
+          <Link href='/terms' className='hover:underline'>
+            Terms
           </Link>
-        </p>
-      </div>
-
-      <div className='flex-grow flex justify-end'>
-        <Link href='/terms' className='hover:underline'>
-          Terms
-        </Link>
-        <span className='mx-1'>|</span>
-        <Link href='/privacy' className='hover:underline'>
-          Privacy
-        </Link>
+          <span className='mx-1'>|</span>
+          <Link href='/privacy' className='hover:underline'>
+            Privacy
+          </Link>
+        </div>
       </div>
     </footer>
   );
