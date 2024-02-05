8741e8909fdf9a3b15b460c7f1a8db0269de128f
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-02-05 06:33:22 -0300

    feat: add footer social links, privacy & terms

--- web/src/modules/shared/components/layout/Footer.tsx
+++ web/src/modules/shared/components/layout/Footer.tsx
@@ -1,13 +1,61 @@
+import {
+  IconDefinition,
+  faDiscord,
+  faGithub,
+  faTwitter,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faEarth,
+  faEnvelope,
+  faGlobe,
+  faHeart,
+  faMusic,
+  faSitemap,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+
+const FooterIcon = ({ icon, href }: { href: string; icon: IconDefinition }) => (
+  <Link
+    href={href}
+    className='text-zinc-500 hover:text-zinc-400 transition-colors duration-150'
+  >
+    <FontAwesomeIcon icon={icon} className='text-2xl' />
+  </Link>
+);
+
 export function Footer() {
   return (
-    <footer className='w-full h-12 flex flex-row justify-center items-center bg-zinc-900 border-t border-zinc-700 p-2 z-10'>
-      <div className='text-sm text-zinc-500'>
+    <footer className='w-full h-14 flex flex-row justify-between items-center border-t border-zinc-700 p-2 z-10 text-sm text-zinc-400'>
+      {/* Social links */}
+      <div className='flex-grow flex flex-row gap-2'>
+        <FooterIcon icon={faTwitter} href='https://twitter.com/OpenNBS' />
+        <FooterIcon icon={faYoutube} href='https://youtube.com/@OpenNBS' />
+        <FooterIcon icon={faGithub} href='https://github.com/OpenNBS' />
+        <FooterIcon icon={faDiscord} href='https://discord.gg' />
+        {/* <FooterIcon icon={faEarth} href='https://discord.gg' /> */}
+        {/* <FooterIcon icon={faEnvelope} href='mailto:opennbs@gmail.com' /> */}
+        {/* <FooterIcon icon={faHeart} href='https://opencollective.com/OpenNBS' /> */}
+      </div>
+
+      <div className=''>
         <p>
           © {new Date().getFullYear()}{' '}
-          <a href='https://opennbs.org/' className='underline'>
+          <Link href='https://opennbs.org/' className='hover:underline'>
             OpenNBS
-          </a>
+          </Link>
         </p>
+      </div>
+
+      <div className='flex-grow flex justify-end'>
+        <Link href='/terms' className='hover:underline'>
+          Terms
+        </Link>
+        <span className='mx-1'>|</span>
+        <Link href='/privacy' className='hover:underline'>
+          Privacy
+        </Link>
       </div>
     </footer>
   );
