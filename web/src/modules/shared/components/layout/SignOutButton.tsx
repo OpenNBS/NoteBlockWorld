8ba1e3ec28b71f0c9c329adc9f423c1bb4db86a8
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-02-09 00:39:16 -0300

    fix: prevent text wrap on sign in button

--- web/src/modules/shared/components/layout/SignOutButton.tsx
+++ web/src/modules/shared/components/layout/SignOutButton.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link';
 export function SignInButton() {
   return (
     <Link href='/login'>
-      <div className='flex justify-between items-center gap-2 px-1 border border-blue-500 text-blue-500 h-8 rounded-full hover:border-blue-400 hover:text-blue-400 transition-colors duration-150'>
+      <div className=' text-nowrap flex justify-between items-center gap-2 px-1 border border-blue-500 text-blue-500 h-8 rounded-full hover:border-blue-400 hover:text-blue-400 transition-colors duration-150'>
         <div className='h-6'>
           <FontAwesomeIcon icon={faUserCircle} className='h-full' />
         </div>
