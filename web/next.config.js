4eccb83f7535eeb70db9dfbbd7e23b7727fe3fc2
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-04-11 01:31:23 -0300

    chore: add Next config for loading external images

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   // See: https://github.com/Automattic/node-canvas/issues/867#issuecomment-1925284985
   webpack: (config) => {
     config.externals.push({ canvas: 'commonjs canvas' });
     return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.THUMBNAIL_URL,
+        port: '',
+      },
+    ],
   },
 };
 
