2e8c39221e49e75c876f56370c142cdb975cbf14
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-04-20 01:59:16 -0300

    fix: huge chevron icon causing reflow on page load

--- web/src/modules/browse/components/client/LoadMoreButton.tsx
+++ web/src/modules/browse/components/client/LoadMoreButton.tsx
@@ -7,7 +7,9 @@ const LoadMoreButton = ({ onClick }: { onClick: () => void }) => {
       onClick={onClick}
       className='flex justify-center col-span-full bg-zinc-800 rounded-lg py-6 md:py-4 sm:py-3 hover:bg-zinc-700 hover:scale-y-110 transition-all'
     >
-      <FontAwesomeIcon icon={faChevronDown} />
+      <i>
+        <FontAwesomeIcon icon={faChevronDown} size='1x' />
+      </i>
     </button>
   );
 };
