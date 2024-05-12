a23e7d3e706f3e158480a32b2c42d05b1cc560d4
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-05-12 05:13:08 -0300

    fix: sign out button not working

--- web/src/modules/auth/components/client/login.util.ts
+++ web/src/modules/auth/components/client/login.util.ts
@@ -6,7 +6,7 @@ export function deleteAuthCookies() {
   // delete cookie
   const cookiesToBeDeleted = ['refresh_token', 'user', 'token'];
   cookiesToBeDeleted.forEach((cookie) => {
-    // document.cookie = `${cookie}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+    document.cookie = `${cookie}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
   });
 }
 
