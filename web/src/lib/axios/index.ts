a913ff898b43ceaac8e8e4a9b897c88dda75567c
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-05-12 05:21:18 -0300

    fix: replace hardcoded API URL with enviroment variable

--- web/src/lib/axios/index.ts
+++ web/src/lib/axios/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseApiURL = 'http://localhost:4000/api/v1';
+export const baseApiURL = process.env.NEXT_PUBLIC_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: baseApiURL,
