7de5b0e17572a1755fc5e105d69cdbc7a0c9ca88
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-05-14 04:25:26 -0300

    feat: add NBW logo to login page box

--- web/src/app/(auth)/login/loginPage.tsx
+++ web/src/app/(auth)/login/loginPage.tsx
@@ -1,5 +1,6 @@
 import { faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Image from 'next/image';
 import Link from 'next/link';
 
 import { baseApiURL } from '@web/src/lib/axios';
@@ -29,28 +30,56 @@ const signIn = (provider: AuthProvider) => {
 export const LoginPage = () => {
   return (
     <main className='w-screen h-screen p-6 text-center text-balance flex items-center justify-center'>
-      <div className='flex flex-col gap-4 bg-zinc-800 w-[480px] rounded-xl p-10'>
-        <div>
-          <h2 className='text-4xl mb-2'>Sign in</h2>
-          <p>to discover, share and listen to note block music</p>
+      <div className='flex flex-col sm:flex-row gap-8 sm:gap-12 bg-zinc-800 w-fit rounded-xl p-10'>
+        {/* Left half */}
+        <div className='flex flex-row sm:flex-col items-center justify-center gap-2'>
+          <Image
+            src='/nbw-color.png'
+            alt='Note Block World logo'
+            className='w-[64px] sm:w-[128px]'
+            width={150}
+            height={150}
+          />
+          <h1 className='text-2xl whitespace-break-spaces w-min font-light text-left sm:text-center leading-none'>
+            Note Block World
+          </h1>
         </div>
-        <div className='h-4'></div>
-        {/* Login with Google */}
-        <Link
-          href={signIn('google')}
-          className='flex items-center text-white outline outline-white outline-1 bg-none p-2.5 hover:outline-orange-500 hover:text-orange-500 hover:transition-all duration-200 uppercase rounded-lg'
-        >
-          <FontAwesomeIcon icon={faGoogle} className='align-middle mr-2 h-5' />
-          <span className='flex-1 text-nowrap'>Login with Google</span>
-        </Link>
-        {/* Login with Github */}
-        <Link
-          href={signIn('github')}
-          className='flex items-center text-white outline outline-white outline-1 bg-none p-2.5 hover:outline-orange-500 hover:text-orange-500 hover:transition-all duration-200 uppercase rounded-lg'
-        >
-          <FontAwesomeIcon icon={faGithub} className='align-middle mr-2 h-5' />
-          <span className='flex-1'>Login with GitHub</span>
-        </Link>
+
+        {/* Vertical divider (mobile) */}
+        <div className='w-[1px] min-h-full hidden sm:block bg-zinc-600'></div>
+        {/* Horizontal divider (desktop) */}
+        <div className='h-[1px] min-w-full block sm:hidden bg-zinc-600'></div>
+
+        {/* Right half */}
+        <div className='flex flex-col gap-4'>
+          <div className='flex flex-col'>
+            <h2 className='text-4xl mb-2'>Sign in</h2>
+            <p>to discover, share and listen to note block music</p>
+          </div>
+          <div className='h-4'></div>
+          {/* Login with Google */}
+          <Link
+            href={signIn('google')}
+            className='flex items-center text-white outline outline-white outline-1 bg-none p-2.5 hover:outline-orange-500 hover:text-orange-500 hover:transition-all duration-200 uppercase rounded-lg'
+          >
+            <FontAwesomeIcon
+              icon={faGoogle}
+              className='align-middle mr-2 h-5'
+            />
+            <span className='flex-1 text-nowrap'>Login with Google</span>
+          </Link>
+          {/* Login with Github */}
+          <Link
+            href={signIn('github')}
+            className='flex items-center text-white outline outline-white outline-1 bg-none p-2.5 hover:outline-orange-500 hover:text-orange-500 hover:transition-all duration-200 uppercase rounded-lg'
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              className='align-middle mr-2 h-5'
+            />
+            <span className='flex-1'>Login with GitHub</span>
+          </Link>
+        </div>
       </div>
     </main>
   );
