ae5c853962462b68410072be5768aaf0f274ac74
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-05-14 18:02:15 -0300

    fix: change note block background to flat and add color back

--- web/src/app/(auth)/layout.tsx
+++ web/src/app/(auth)/layout.tsx
@@ -9,9 +9,9 @@ export default async function LoginLayout({
   return (
     <>
       <div
-        className="fixed z-[-1] w-full h-full bg-center bg-repeat grayscale before:content-[''] before:absolute before:w-full before:h-full before:bg-gradient-to-b before:from-black/20 before:to-black/80"
+        className="fixed z-[-1] w-full h-full bg-center bg-repeat grayscale-[50%] before:content-[''] before:absolute before:w-full before:h-full before:bg-gradient-to-b before:from-black/20 before:to-black/90"
         style={{
-          backgroundImage: "url('/background-tile.png')",
+          backgroundImage: "url('/background-tile-flat.png')",
         }}
       ></div>
       {children}
