b27b2dcf5a1c0197253280ea7ca2ec35769ca712
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-05-16 01:06:56 -0300

    refactor: move login buttons to their own group within div

--- web/src/app/(auth)/login/loginPage.tsx
+++ web/src/app/(auth)/login/loginPage.tsx
@@ -60,29 +60,31 @@ export const LoginPage = () => {
             <h2 className='text-4xl mb-2'>Sign in</h2>
             <p>to discover, share and listen to note block music</p>
           </div>
-          <div className='h-4'></div>
-          {/* Login with Google */}
-          <Link
-            href={signIn('google')}
-            className='flex items-center text-white outline outline-white outline-1 bg-none p-2.5 hover:outline-orange-500 hover:text-orange-500 hover:transition-all duration-200 uppercase rounded-lg'
-          >
-            <FontAwesomeIcon
-              icon={faGoogle}
-              className='align-middle mr-2 h-5'
-            />
-            <span className='flex-1 text-nowrap'>Login with Google</span>
-          </Link>
-          {/* Login with Github */}
-          <Link
-            href={signIn('github')}
-            className='flex items-center text-white outline outline-white outline-1 bg-none p-2.5 hover:outline-orange-500 hover:text-orange-500 hover:transition-all duration-200 uppercase rounded-lg'
-          >
-            <FontAwesomeIcon
-              icon={faGithub}
-              className='align-middle mr-2 h-5'
-            />
-            <span className='flex-1'>Login with GitHub</span>
-          </Link>
+
+          <div className='flex flex-col w-full gap-4'>
+            {/* Login with Google */}
+            <Link
+              href={signIn('google')}
+              className='flex items-center text-white outline outline-white outline-1 bg-none p-2.5 hover:outline-blue-500 hover:text-blue-500 hover:transition-all duration-200 uppercase rounded-lg'
+            >
+              <FontAwesomeIcon
+                icon={faGoogle}
+                className='align-middle mr-2 h-5'
+              />
+              <span className='flex-1 text-nowrap'>Login with Google</span>
+            </Link>
+            {/* Login with Github */}
+            <Link
+              href={signIn('github')}
+              className='flex items-center text-white outline outline-white outline-1 bg-none p-2.5 hover:outline-blue-500 hover:text-blue-500 hover:transition-all duration-200 uppercase rounded-lg'
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                className='align-middle mr-2 h-5'
+              />
+              <span className='flex-1'>Login with GitHub</span>
+            </Link>
+          </div>
         </div>
       </div>
     </main>
