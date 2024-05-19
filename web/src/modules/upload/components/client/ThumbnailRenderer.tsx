405c50af000ffb203305549bdda4e3a92d43a17a
tomast1337 <vycasnicolas@gmail.com>
2024-05-18 21:32:38 -0300

    Refactor ThumbnailRendererCanvas component

--- web/src/modules/upload/components/client/ThumbnailRenderer.tsx
+++ web/src/modules/upload/components/client/ThumbnailRenderer.tsx
@@ -1,25 +1,31 @@
 import type { Note } from '@shared/features/thumbnail';
 import { drawNotesOffscreen, swap } from '@shared/features/thumbnail';
 import { useEffect, useRef, useState } from 'react';
+import { UseFormReturn } from 'react-hook-form';
+
+import { UploadSongForm } from './uploadSongForm.zod';
 
 type ThumbnailRendererCanvasProps = {
   notes: Note[];
-  zoomLevel: number;
-  startTick: number;
-  startLayer: number;
-  backgroundColor: string;
+  formMethods: UseFormReturn<UploadSongForm>;
 };
 
 export const ThumbnailRendererCanvas = ({
   notes,
-  zoomLevel,
-  startTick,
-  startLayer,
-  backgroundColor,
+  formMethods,
 }: ThumbnailRendererCanvasProps) => {
   const canvasRef = useRef(null);
   const drawRequest = useRef<number | null>(null);
   const [loading, setLoading] = useState(true);
+
+  const [zoomLevel, startTick, startLayer, backgroundColor] = formMethods.watch(
+    [
+      'coverData.zoomLevel',
+      'coverData.startTick',
+      'coverData.startLayer',
+      'coverData.backgroundColor',
+    ],
+  );
 
   useEffect(() => {
     const canvas = canvasRef.current as HTMLCanvasElement | null;
@@ -28,6 +34,64 @@ export const ThumbnailRendererCanvas = ({
     // Set canvas size to match the container size
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.width / (1280 / 768);
+    /*
+    // on mouse scroll event
+    canvas.addEventListener('wheel', (e) => {
+      const delta = e.deltaY;
+      // do something with delta
+      const maxZoom = 5;
+      const minZoom = 1;
+      const zoomSpeed = 0.1;
+      const newZoom = zoomLevel + delta * zoomSpeed;
+      formMethods.setValue(
+        'coverData.zoomLevel',
+        Math.min(Math.max(newZoom, minZoom), maxZoom),
+      );
+    });
+
+    // on mouse drag event
+    canvas.addEventListener('mousedown', (e) => {
+      const mouse = {
+        x: e.clientX,
+        y: e.clientY,
+      };
+
+      const minLayer = 0;
+      const maxLayer = 3;
+
+      const minTick = 0;
+      const maxTick = 1000;
+
+      canvas.addEventListener('mousemove', (e) => {
+        const dx = e.clientX - mouse.x;
+        const dy = e.clientY - mouse.y;
+        mouse.x = e.clientX;
+        mouse.y = e.clientY;
+        // do something with dx and dy
+
+        const newTick = startTick - dy;
+        const newLayer = startLayer + dx;
+
+        formMethods.setValue(
+          'coverData.startTick',
+          Math.min(Math.max(newTick, minTick), maxTick),
+        );
+
+        formMethods.setValue(
+          'coverData.startLayer',
+          Math.min(Math.max(newLayer, minLayer), maxLayer),
+        );
+      });
+    });
+    canvas.addEventListener('mouseup', () => {
+      canvas.removeEventListener('mousemove', () => undefined);
+    });
+    return () => {
+      canvas.removeEventListener('mousedown', () => undefined);
+      canvas.removeEventListener('mouseup', () => undefined);
+      canvas.removeEventListener('wheel', () => undefined);
+    };
+    */
   }, []);
 
   useEffect(() => {
