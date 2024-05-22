b61984f010b0ae72f985539b5608347613306a00
tomast1337 <vycasnicolas@gmail.com>
2024-05-21 21:48:05 -0300

    Update FormElements component to handle invalid state
    Co-authored-by: Bernardo Costa <Bentroen@users.noreply.github.com>

--- web/src/modules/upload/components/client/FormElements.tsx
+++ web/src/modules/upload/components/client/FormElements.tsx
@@ -4,13 +4,17 @@ import { cn } from '@web/src/lib/tailwind.utils';
 
 export const Input = forwardRef<
   HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
+  React.InputHTMLAttributes<HTMLInputElement> & {
+    invalid?: boolean;
+  }
 >((props, ref) => {
   return (
     <input
       ref={ref}
       {...props}
-      className={`block h-12 w-full rounded-lg bg-transparent border-2 border-zinc-500 disabled:border-zinc-700 disabled:cursor-not-allowed disabled:text-zinc-500 p-2`}
+      className={`block h-12 w-full rounded-lg bg-transparent border-2 ${
+        props.invalid ? 'border-red-500' : 'border-zinc-500'
+      } disabled:border-zinc-700 disabled:cursor-not-allowed disabled:text-zinc-500 invalid:bg-red-500 p-2`}
     />
   );
 });
@@ -18,14 +22,18 @@ Input.displayName = 'Input';
 
 export const Select = forwardRef<
   HTMLSelectElement,
-  React.SelectHTMLAttributes<HTMLSelectElement>
+  React.SelectHTMLAttributes<HTMLSelectElement> & {
+    invalid?: boolean;
+  }
 >((props, ref) => {
   return (
     <select
       ref={ref}
       {...props}
       className={cn(
-        `block h-12 w-full rounded-lg bg-transparent border-2 border-zinc-500 disabled:border-zinc-700 disabled:cursor-not-allowed disabled:text-zinc-500 p-2`,
+        `block h-12 w-full rounded-lg bg-transparent border-2 ${
+          props.invalid ? 'border-red-500' : 'border-zinc-500'
+        }  disabled:border-zinc-700 disabled:cursor-not-allowed disabled:text-zinc-500 p-2`,
         props.className,
       )}
     />
