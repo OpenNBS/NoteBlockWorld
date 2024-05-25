a25b60c0549ca0dba40c398858746b103440cd27
tomast1337 <vycasnicolas@gmail.com>
2024-05-25 20:15:28 -0300

    Fix cookie deletion in login.util.ts

--- web/src/modules/auth/components/client/login.util.ts
+++ web/src/modules/auth/components/client/login.util.ts
@@ -6,6 +6,7 @@ export function deleteAuthCookies() {
   // delete cookie
   const cookiesToBeDeleted = ['refresh_token', 'user', 'token'];
   cookiesToBeDeleted.forEach((cookie) => {
+    if (!document) return;
     document.cookie = `${cookie}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
   });
 }
