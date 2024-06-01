b998deb0bc43a742f9c100918d6da94c13c518d2
tomast1337 <vycasnicolas@gmail.com>
2024-06-01 18:54:26 -0300

    refactor: Remove unnecessary SongPageView types
    The SongPageView and SongPageViewUploader types in the song.type.ts file have been removed as they are no longer needed. This refactor simplifies the codebase and improves maintainability.

--- web/src/modules/song/types/song.type.ts
+++
@@ -1,5 +0,0 @@
-import { SongViewDtoType } from '@shared/validation/song/dto/types';
-
-export type SongPageViewUploader = SongViewDtoType['uploader'];
-
-export type SongPageView = SongViewDtoType;
