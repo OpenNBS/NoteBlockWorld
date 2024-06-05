97bbf0a2c19ae74f924b755740e3485de76c02b7
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-06-05 06:04:40 -0300

    fix: readd user menu shadow

--- web/src/modules/shared/components/layout/UserMenu.tsx
+++ web/src/modules/shared/components/layout/UserMenu.tsx
@@ -25,61 +25,69 @@ export function UserMenu({ userData }: { userData: LoggedUserData }) {
         <UserMenuButton userData={userData} />
       </PopoverTrigger>
       <PopoverContent
-        className='w-fit p-[2px] pb-2 h-fit box-border shadow-[inset_0px_0px_0px_2px_rgb(82_82_91)] border-zinc-600 bg-zinc-800 text-white rounded-lg'
+        className='w-fit p-[2px] h-fit box-border shadow-[inset_0px_0px_0px_2px_rgb(82_82_91)] border-zinc-600 bg-zinc-800 text-white rounded-lg'
         sideOffset={10}
         align='end'
         alignOffset={-10}
         arrowPadding={10}
       >
-        <PopoverArrow className='fill-zinc-600' />
-        <div className='min-w-48 max-w-64'>
-          {/* User */}
-          <div className='flex flex-row gap-2 items-center p-4 pb-2'>
-            <div className='h-8 w-8'>
-              <Image
-                width={32}
-                height={32}
-                src={userData.profileImage}
-                alt=''
-                className='rounded-full'
+        <div className='shadow-xl pb-2'>
+          <PopoverArrow className='fill-zinc-600' />
+          <div className='min-w-48 max-w-64'>
+            {/* User */}
+            <div className='flex flex-row gap-2 items-center p-4 pb-2'>
+              <div className='h-8 w-8'>
+                <Image
+                  width={32}
+                  height={32}
+                  src={userData.profileImage}
+                  alt=''
+                  className='rounded-full'
+                />
+              </div>
+              <div className='flex-shrink min-w-0 flex flex-col leading-tight'>
+                <h4 className='truncate font-semibold'>{userData.username}</h4>
+                <p className='text-zinc-300 text-xs truncate'>
+                  {userData.email}
+                </p>
+              </div>
+            </div>
+
+            <UserMenuSplitLine />
+
+            {/* Links */}
+            <div className='flex flex-col'>
+              {/* <UserMenuLink href='/my-profile' icon={faUser} label='Profile' /> */}
+              <UserMenuLink href='/my-songs' icon={faMusic} label='My songs' />
+              <UserMenuLink
+                href='/logout'
+                icon={faSignOutAlt}
+                label='Sign out'
               />
             </div>
-            <div className='flex-shrink min-w-0 flex flex-col leading-tight'>
-              <h4 className='truncate font-semibold'>{userData.username}</h4>
-              <p className='text-zinc-300 text-xs truncate'>{userData.email}</p>
+
+            <UserMenuSplitLine />
+
+            <div className='flex flex-col'>
+              <UserMenuLink
+                href='https://github.com/OpenNBS/NoteBlockWorld'
+                icon={faGithub}
+                label='GitHub'
+                external
+              />
+              <UserMenuLink
+                href='https://github.com/orgs/OpenNBS/projects/4'
+                icon={faListCheck}
+                label='Roadmap'
+                external
+              />
+              <UserMenuLink
+                href='https://opencollective.com/opennbs/donate/profile'
+                icon={faHeart}
+                label='Donate'
+                external
+              />
             </div>
-          </div>
-
-          <UserMenuSplitLine />
-
-          {/* Links */}
-          <div className='flex flex-col'>
-            {/* <UserMenuLink href='/my-profile' icon={faUser} label='Profile' /> */}
-            <UserMenuLink href='/my-songs' icon={faMusic} label='My songs' />
-            <UserMenuLink href='/logout' icon={faSignOutAlt} label='Sign out' />
-          </div>
-
-          <UserMenuSplitLine />
-
-          <div className='flex flex-col'>
-            <UserMenuLink
-              href='https://github.com/OpenNBS/NoteBlockWorld'
-              icon={faGithub}
-              label='GitHub'
-              external
-            />
-            <UserMenuLink
-              href='https://github.com/orgs/OpenNBS/projects/4'
-              icon={faListCheck}
-              label='Roadmap'
-              external
-            />
-            <UserMenuLink
-              href='https://opencollective.com/opennbs/donate/profile'
-              icon={faHeart}
-              label='Donate'
-              external
-            />
           </div>
         </div>
       </PopoverContent>
