0025b9f58365d4e0140b28ee9776fcc88e5547be
tomast1337 <vycasnicolas@gmail.com>
2024-06-06 22:42:46 -0300

    Removed unused functions

--- web/src/app/(content)/browse/page.tsx
+++ web/src/app/(content)/browse/page.tsx
@@ -1,7 +1,5 @@
 import { PageQueryDTOType } from '@shared/validation/common/dto/types';
-import { SongPreviewDtoType } from '@shared/validation/song/dto/types';
 
-import axiosInstance from '@web/src/lib/axios';
 import { HomePageProvider } from '@web/src/modules/browse/components/client/context/HomePage.context';
 import { HomePageComponent } from '@web/src/modules/browse/components/HomePageComponent';
 
@@ -18,29 +16,6 @@ const featuredSongsParams: PageQueryDTOType = {
   order: false,
   timespan: 'week',
 };
-async function fetchRecentSongs(): Promise<SongPreviewDtoType[]> {
-  try {
-    const response = await axiosInstance.get('/song', {
-      params: recentSongsParams,
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error loading recent songs:', error);
-    return [];
-  }
-}
-
-async function fetchFeaturedSongs(): Promise<SongPreviewDtoType[]> {
-  try {
-    const response = await axiosInstance.get('/song', {
-      params: featuredSongsParams,
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error loading featured songs:', error);
-    return [];
-  }
-}
 
 async function Home() {
   //const recentSongs = await fetchRecentSongs();
