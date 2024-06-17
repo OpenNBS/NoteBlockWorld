e99c518d17f105258dcfe9bc9517b92461767f5f
Bernardo Costa <bemcdc@gmail.com>
2024-06-17 15:28:06 -0300

    chore: remove `TokenRemover` component from login page

--- web/src/app/(auth)/login/page.tsx
+++ web/src/app/(auth)/login/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from 'next/navigation';
 
 import { LoginPage } from '@web/src/modules/auth/components/loginPage';
-import { TokenRemover } from '@web/src/modules/auth/components/TokenRemover';
 import { checkLogin } from '@web/src/modules/auth/features/auth.utils';
 
 const Login = async () => {
@@ -12,7 +11,7 @@ const Login = async () => {
   return (
     <>
       <LoginPage />
-      <TokenRemover />
+      {/* <TokenRemover /> */}
     </>
   );
 };
