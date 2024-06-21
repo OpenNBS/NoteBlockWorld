9bd09f04d648f3693a78ba0d6ed0eaab7fdb2e3b
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-06-21 02:58:51 -0300

    refactor: make `SongView` DTO type-safe and validate with class-validator

--- shared/validation/song/dto/SongView.dto.ts
+++ shared/validation/song/dto/SongView.dto.ts
@@ -1,3 +1,12 @@
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUrl,
+} from 'class-validator';
+
 import { SongStats } from '@shared/features/song/SongStats';
 
 import { SongDocument } from '../../../../server/src/song/entity/song.entity';
@@ -8,36 +17,86 @@ export type SongViewUploader = {
 };
 
 export class SongViewDto {
+  @IsString()
+  @IsNotEmpty()
   publicId: string;
-  createdAt: Date;
-  editedAt: Date;
-  uploader: SongViewUploader;
-  playCount: number;
-  downloadCount: number;
-  likeCount: number;
-  allowDownload: boolean;
-  visibility: string;
 
-  // SONG ATTRIBUTES
+  @IsDate()
+  @IsNotEmpty()
+  createdAt: Date;
+
+  @IsNotEmpty()
+  uploader: SongViewUploader;
+
+  @IsUrl()
+  @IsNotEmpty()
+  thumbnailUrl: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  playCount: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  downloadCount: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  likeCount: number;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  allowDownload: boolean;
+
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
   originalAuthor: string;
+
+  @IsString()
   description: string;
 
-  // SONG STATS
-  songStats: SongStats;
+  @IsString()
+  @IsNotEmpty()
+  category: string;
+
+  @IsString()
+  @IsNotEmpty()
+  license: string;
+
+  customInstruments: string[];
+
+  @IsNumber()
+  @IsNotEmpty()
+  fileSize: number;
+
+  @IsNotEmpty()
+  stats: SongStats;
 
   public static fromSongDocument(song: SongDocument): SongViewDto {
-    const data = song.toJSON();
-
-    // TODO: this is not type-safe
-
     return new SongViewDto({
-      ...data,
+      publicId: song.publicId,
+      createdAt: song.createdAt,
       uploader: song.uploader as unknown as SongViewUploader,
+      thumbnailUrl: song.thumbnailUrl,
+      playCount: song.playCount,
+      downloadCount: song.downloadCount,
+      likeCount: song.likeCount,
+      allowDownload: song.allowDownload,
+      title: song.title,
+      originalAuthor: song.originalAuthor,
+      description: song.description,
+      category: song.category,
+      license: song.license,
+      customInstruments: song.customInstruments,
+      fileSize: song.fileSize,
+      stats: song.stats,
     });
   }
 
-  constructor(song: Partial<SongViewDto>) {
+  constructor(song: SongViewDto) {
     Object.assign(this, song);
   }
 }
