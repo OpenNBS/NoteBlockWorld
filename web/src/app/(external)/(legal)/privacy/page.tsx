a686449461dbde7c3c94264f310ed41babe5c75f
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-06-24 04:15:52 -0300

    fix: add layout for document pages

--- web/src/app/(external)/(legal)/privacy/page.tsx
+++ web/src/app/(external)/(legal)/privacy/page.tsx
@@ -7,7 +7,13 @@ async function PrivacyPolicyPage() {
   const fullPath = path.join('./public/privacy.md');
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
-  return <CustomMarkdown MarkdownContent={fileContents} />;
+  return (
+    <div className='md:py-[8rem]'>
+      <div className='p-8 mx-auto max-w-screen-lg md:rounded-3xl bg-zinc-950/50 backdrop-blur-[10px]'>
+        <CustomMarkdown MarkdownContent={fileContents} />
+      </div>
+    </div>
+  );
 }
 
 export default PrivacyPolicyPage;
