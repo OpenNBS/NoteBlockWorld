c600f99a6d0fdfd019161340ec7f2217d163c588
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-06-24 03:22:47 -0300

    refactor: move custom markdown components to module-scope functions

--- web/src/modules/shared/components/CustomMarkdown.tsx
+++ web/src/modules/shared/components/CustomMarkdown.tsx
@@ -12,91 +12,23 @@ export const CustomMarkdown = ({
   return (
     <>
       <Header />
-      <article
-        style={{
-          padding: '8rem 1rem',
-          maxWidth: '800px',
-          minHeight: '100vh',
-          margin: '0 auto',
-          fontSize: '1.25rem',
-        }}
-      >
+      <article className='p-8 max-w-800 mx-auto min-h-screen text-lg'>
         <Markdown
           components={{
-            p: ({ node, ...props }) => (
-              <p {...props} style={{ lineHeight: 1.6 }} />
-            ),
-            h1: ({ node, ...props }) => (
-              <h1 {...props} style={{ fontSize: '2rem', fontWeight: 'bold' }} />
-            ),
-            h2: ({ node, ...props }) => (
-              <h2
-                {...props}
-                style={{ fontSize: '1.5rem', fontWeight: 'bold' }}
-              />
-            ),
-            h3: ({ node, ...props }) => (
-              <h3
-                {...props}
-                style={{ fontSize: '1.25rem', fontWeight: 'bold' }}
-              />
-            ),
-            h4: ({ node, ...props }) => (
-              <h4 {...props} style={{ fontSize: '1rem', fontWeight: 'bold' }} />
-            ),
-            h5: ({ node, ...props }) => (
-              <h5
-                {...props}
-                style={{ fontSize: '0.875rem', fontWeight: 'bold' }}
-              />
-            ),
-            h6: ({ node, ...props }) => (
-              <h6
-                {...props}
-                style={{ fontSize: '0.75rem', fontWeight: 'bold' }}
-              />
-            ),
-            ul: ({ node, ...props }) => (
-              <ul {...props} style={{ paddingLeft: '1rem' }} />
-            ),
-            ol: ({ node, ...props }) => (
-              <ol {...props} style={{ paddingLeft: '1rem' }} />
-            ),
-            li: ({ node, ...props }) => (
-              <li {...props} style={{ lineHeight: 1.6 }} />
-            ),
-            blockquote: ({ node, ...props }) => (
-              <blockquote
-                {...props}
-                style={{ borderLeft: '2px solid #333', paddingLeft: '1rem' }}
-              />
-            ),
-            pre: ({ node, ...props }) => (
-              <pre
-                {...props}
-                style={{
-                  backgroundColor: '#333',
-                  color: '#fff',
-                  padding: '1rem',
-                  overflowX: 'auto',
-                }}
-              />
-            ),
-            code: ({ node, ...props }) => (
-              <code
-                {...props}
-                style={{
-                  backgroundColor: '#333',
-                  color: '#fff',
-                  padding: '0.25rem',
-                }}
-              />
-            ),
-            a: ({ node, ...props }) => (
-              <Link href={props.href as string} passHref>
-                {props.children}
-              </Link>
-            ),
+            p,
+            h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            h6,
+            ul,
+            ol,
+            li,
+            blockquote,
+            pre,
+            code,
+            a,
           }}
         >
           {MarkdownContent}
@@ -106,3 +38,153 @@ export const CustomMarkdown = ({
     </>
   );
 };
+
+function p({
+  node,
+  ...props
+}: {
+  node: React.ReactNode;
+  props: React.ComponentPropsWithoutRef<'p'>;
+}) {
+  return <p {...props} className='leading-6' />;
+}
+
+function h1({
+  node,
+  ...props
+}: {
+  node: React.ReactNode;
+  props: React.ComponentPropsWithoutRef<'h1'>;
+}) {
+  return <p {...props} className='text-2xl font-bold' />;
+}
+
+function h2({
+  node,
+  ...props
+}: {
+  node: React.ReactNode;
+  props: React.ComponentPropsWithoutRef<'h2'>;
+}) {
+  return <p {...props} className='text-2xl font-bold' />;
+}
+
+function h3({
+  node,
+  ...props
+}: {
+  node: React.ReactNode;
+  props: React.ComponentPropsWithoutRef<'h3'>;
+}) {
+  return <p {...props} className='text-lg font-bold' />;
+}
+
+function h4({
+  node,
+  ...props
+}: {
+  node: React.ReactNode;
+  props: React.ComponentPropsWithoutRef<'h4'>;
+}) {
+  return <p {...props} className='text-base font-bold' />;
+}
+
+function h5({
+  node,
+  ...props
+}: {
+  node: React.ReactNode;
+  props: React.ComponentPropsWithoutRef<'h5'>;
+}) {
+  return <p {...props} className='text-sm font-bold' />;
+}
+
+function h6({
+  node,
+  ...props
+}: {
+  node: React.ReactNode;
+  props: React.ComponentPropsWithoutRef<'h6'>;
+}) {
+  return <p {...props} className='text-xs font-bold' />;
+}
+
+function ul({
+  node,
+  ...props
+}: {
+  node: React.ReactNode;
+  props: React.ComponentPropsWithoutRef<'ul'>;
+}) {
+  return <p {...props} className='pl-4' />;
+}
+
+function ol({
+  node,
+  ...props
+}: {
+  node: React.ReactNode;
+  props: React.ComponentPropsWithoutRef<'ol'>;
+}) {
+  return <p {...props} className='pl-4' />;
+}
+
+function li({
+  node,
+  ...props
+}: {
+  node: React.ReactNode;
+  props: React.ComponentPropsWithoutRef<'li'>;
+}) {
+  return <p {...props} className='leading-6' />;
+}
+
+function blockquote({
+  node,
+  ...props
+}: {
+  node: React.ReactNode;
+  props: React.ComponentPropsWithoutRef<'blockquote'>;
+}) {
+  return <p {...props} className='border-l-2 pl-4' />;
+}
+
+function pre({
+  node,
+  ...props
+}: {
+  node: React.ReactNode;
+  props: React.ComponentPropsWithoutRef<'pre'>;
+}) {
+  return (
+    <p {...props} className='bg-gray-900 text-white p-4 overflow-x-auto' />
+  );
+}
+
+function code({
+  node,
+  ...props
+}: {
+  node: React.ReactNode;
+  props: React.ComponentPropsWithoutRef<'code'>;
+}) {
+  return <p {...props} className='bg-gray-900 text-white p-1' />;
+}
+
+function a({
+  node,
+  children,
+  href,
+  ...props
+}: {
+  node: React.ReactNode;
+  children: React.ReactNode;
+  href: string;
+  props: React.ComponentPropsWithoutRef<'a'>;
+}) {
+  return (
+    <Link {...props} href={href} className=''>
+      {children}
+    </Link>
+  );
+}
