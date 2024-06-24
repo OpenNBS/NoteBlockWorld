8d86887bee57bbe75830cc47fc2ad9eb6445ab0c
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-06-24 17:18:43 -0300

    fix: add NBW logo to document layout

--- web/src/modules/shared/components/layout/DocumentLayout.tsx
+++ web/src/modules/shared/components/layout/DocumentLayout.tsx
@@ -1,7 +1,14 @@
+import { NoteBlockWorldLogo } from '../NoteBlockWorldLogo';
+
 const DocumentLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='md:py-[8rem]'>
       <div className='p-8 mx-auto max-w-screen-lg md:rounded-3xl bg-zinc-950/50 backdrop-blur-[10px]'>
+        <NoteBlockWorldLogo
+          glow={false}
+          orientation={'horizontal'}
+          size={150}
+        />
         {children}
       </div>
     </div>
