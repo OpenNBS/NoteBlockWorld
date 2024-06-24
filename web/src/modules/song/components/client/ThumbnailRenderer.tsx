760f0b54c4fd683a4c407491e213a011d884488f
tomast1337 <vycasnicolas@gmail.com>
2024-06-23 22:57:23 -0300

    refactor: Remove unused imports and types in SongForm and SongThumbnailInput components
    Co-authored-by: Bernardo Costa <Bentroen@users.noreply.github.com>

--- web/src/modules/song/components/client/ThumbnailRenderer.tsx
+++ web/src/modules/song/components/client/ThumbnailRenderer.tsx
@@ -1,4 +1,4 @@
-import type { Note } from '@shared/features/thumbnail';
+import { NoteQuadTree } from '@shared/features/song/notes';
 import { drawNotesOffscreen, swap } from '@shared/features/thumbnail';
 import { useEffect, useRef, useState } from 'react';
 import { UseFormReturn } from 'react-hook-form';
@@ -6,7 +6,7 @@ import { UseFormReturn } from 'react-hook-form';
 import { UploadSongForm } from './SongForm.zod';
 
 type ThumbnailRendererCanvasProps = {
-  notes: Note[];
+  notes: NoteQuadTree;
   formMethods: UseFormReturn<UploadSongForm>;
 };
 
@@ -34,64 +34,12 @@ export const ThumbnailRendererCanvas = ({
     // Set canvas size to match the container size
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.width / (1280 / 768);
-    /*
-    // on mouse scroll event
-    canvas.addEventListener('wheel', (e) => {
-      const delta = e.deltaY;
-      // do something with delta
-      const maxZoom = 5;
-      const minZoom = 1;
-      const zoomSpeed = 0.1;
-      const newZoom = zoomLevel + delta * zoomSpeed;
-      formMethods.setValue(
-        'thumbnailData.zoomLevel',
-        Math.min(Math.max(newZoom, minZoom), maxZoom),
-      );
-    });
 
-    // on mouse drag event
-    canvas.addEventListener('mousedown', (e) => {
-      const mouse = {
-        x: e.clientX,
-        y: e.clientY,
-      };
-
-      const minLayer = 0;
-      const maxLayer = 3;
-
-      const minTick = 0;
-      const maxTick = 1000;
-
-      canvas.addEventListener('mousemove', (e) => {
-        const dx = e.clientX - mouse.x;
-        const dy = e.clientY - mouse.y;
-        mouse.x = e.clientX;
-        mouse.y = e.clientY;
-        // do something with dx and dy
-
-        const newTick = startTick - dy;
-        const newLayer = startLayer + dx;
-
-        formMethods.setValue(
-          'thumbnailData.startTick',
-          Math.min(Math.max(newTick, minTick), maxTick),
-        );
-
-        formMethods.setValue(
-          'thumbnailData.startLayer',
-          Math.min(Math.max(newLayer, minLayer), maxLayer),
-        );
-      });
-    });
-    canvas.addEventListener('mouseup', () => {
-      canvas.removeEventListener('mousemove', () => undefined);
-    });
     return () => {
-      canvas.removeEventListener('mousedown', () => undefined);
-      canvas.removeEventListener('mouseup', () => undefined);
-      canvas.removeEventListener('wheel', () => undefined);
+      if (drawRequest.current) {
+        cancelAnimationFrame(drawRequest.current);
+      }
     };
-    */
   }, []);
 
   useEffect(() => {
