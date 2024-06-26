72d932e2676bdbbaf47892a8e7d3acb95cb2b6a1
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-06-26 04:25:15 -0300

    refactor: break obfuscate function into `SongObfuscator` class

--- shared/features/song/obfuscate.ts
+++ shared/features/song/obfuscate.ts
@@ -1,87 +1,107 @@
-import { Note, Song } from '@encode42/nbs.js';
+import { Layer, Note, Song } from '@encode42/nbs.js';
 
-export function obfuscateSong(song: Song): Song {
-  const output = new Song();
+export class SongObfuscator {
+  private song: Song;
+  private output: Song;
 
-  // ✅ Clear work stats
-  // ✅ Copy: title, author, description, loop info, time signature
+  public static obfuscateSong(song: Song) {
+    return new SongObfuscator(song).song;
+  }
 
-  output.meta.name = song.meta.name;
-  output.meta.author = song.meta.author;
-  output.meta.description = song.meta.description;
+  private constructor(song: Song) {
+    this.song = song;
+    this.output = this.generateObfuscatedSong();
+  }
 
-  output.loop.enabled = song.loop.enabled;
-  output.loop.startTick = song.loop.startTick;
-  output.loop.totalLoops = song.loop.totalLoops;
+  private generateObfuscatedSong(): Song {
+    const song = this.song;
+    const output = new Song();
 
-  output.tempo = song.tempo;
-  output.timeSignature = song.timeSignature;
+    // ✅ Clear work stats
+    // ✅ Copy: title, author, description, loop info, time signature
+    this.copyMetaAndStats(song, output);
+    this.processNotes(song, output);
 
-  // TODO: Remove unused instruments
-  // TODO: Remove instrument info (name, press) - keep sound hash and pitch
+    return output;
+  }
 
-  const noteCountPerInstrument = {};
-  const tempoChangerIds: number[] = [];
+  private copyMetaAndStats(song: Song, output: Song) {
+    output.meta.name = song.meta.name;
+    output.meta.author = song.meta.author;
+    output.meta.description = song.meta.description;
 
-  const lastLayerInTick = new Map<number, number>();
+    output.loop.enabled = song.loop.enabled;
+    output.loop.startTick = song.loop.startTick;
+    output.loop.totalLoops = song.loop.totalLoops;
 
-  const instrumentMapping: Record<number, number> = {
-    1: 1,
-    2: 2,
-    3: 3,
-    4: 4,
-    5: 5,
-    6: 6,
-    7: 7,
-    8: 8,
-    9: 9,
-    10: 10,
-    11: 11,
-  };
+    output.tempo = song.tempo;
+    output.timeSignature = song.timeSignature;
+  }
 
-  // ✅ Pile notes at the top
-  // ✅ Bake layer volume into note velocity
-  // ✅ Bake layer pan into note pan
-  // ✅ Flatten note key and pitch
-  // ✅ Remove silent notes (except Tempo Changers)
-  // ✅ Remove everything in locked layers
-  // ✅ Remove all layer info (name, volume, panning)
+  private processInstruments(song: Song, output: Song) {
+    // TODO: Remove unused instruments
+    // TODO: Remove instrument info (name, press) - keep sound hash and pitch
 
-  for (const [layerId, layer] of song.layers.entries()) {
-    if (layer.isLocked || layer.volume === 0) {
-      continue;
-    }
+    const noteCountPerInstrument = {};
+    const tempoChangerIds: number[] = [];
 
-    console.log('Processing layer', layerId);
+    const lastLayerInTick = new Map<number, number>();
 
-    for (const tickStr in layer.notes) {
-      const note = layer.notes[tickStr];
-      const tick = parseInt(tickStr);
+    const instrumentMapping: Record<number, number> = {
+      1: 1,
+      2: 2,
+      3: 3,
+      4: 4,
+      5: 5,
+      6: 6,
+      7: 7,
+      8: 8,
+      9: 9,
+      10: 10,
+      11: 11,
+    };
+  }
 
-      const isTempoChanger = tempoChangerIds.includes(note.instrument); // TODO: doesn't work yet
+  private processNotes(
+    tempoChangerIds: number[],
+    lastLayerInTick: Map<number, number>,
+  ) {
+    // ✅ Pile notes at the top
+    // ✅ Bake layer volume into note velocity
+    // ✅ Bake layer pan into note pan
+    // ✅ Flatten note key and pitch
+    // ✅ Remove silent notes (except Tempo Changers)
+    // ✅ Remove everything in locked layers
+    // ✅ Remove all layer info (name, volume, panning)
 
-      if (note.velocity === 0 && !isTempoChanger) continue;
-
-      // Key + pitch
+    const resolveKeyAndPitch = (note: Note) => {
       const factoredPitch = note.key * 100 + note.pitch;
       const key = factoredPitch / 100;
       const pitch = factoredPitch % 100;
 
-      // Instrument
-      let instrument;
+      return { key, pitch };
+    };
 
-      if (note.instrument < song.instruments.firstCustomIndex) {
-        instrument = note.instrument;
+    const resolveInstrument = (note: Note) => {
+      let instrumentId;
+
+      if (note.instrument < this.song.instruments.firstCustomIndex) {
+        instrumentId = note.instrument;
       } else {
-        continue;
         // TODO: instrument = instrumentMapping[note.instrument];
       }
 
-      // Velocity
+      return instrumentId;
+    };
+
+    const resolveVelocity = (note: Note, layer: Layer) => {
       let velocity = ((note.velocity / 100) * layer.volume) / 100;
       velocity = Math.round(velocity * 100);
 
-      // Panning
+      return velocity;
+    };
+
+    const resolvePanning = (note: Note, layer: Layer) => {
       let panning;
 
       if (layer.stereo === 0) {
@@ -89,6 +109,15 @@ export function obfuscateSong(song: Song): Song {
       } else {
         panning = (note.panning + layer.stereo) / 2;
       }
+
+      return panning;
+    };
+
+    const getObfuscatedNote = (note: Note, layer: Layer) => {
+      const { key, pitch } = resolveKeyAndPitch(note);
+      const instrument = resolveInstrument(note);
+      const velocity = resolveVelocity(note, layer);
+      const panning = resolvePanning(note, layer);
 
       // Create new note
       const newNote = new Note(instrument, {
@@ -98,7 +127,14 @@ export function obfuscateSong(song: Song): Song {
         pitch,
       });
 
-      // Add to output
+      return newNote;
+    };
+
+    const addNoteToOutput = (tick: number, note: Note) => {
+      // Adds a note at tick at the first row that does not have a note yet
+
+      const output = this.output;
+
       let layerIdToAddNoteTo = lastLayerInTick.get(tick);
 
       if (layerIdToAddNoteTo === undefined) {
@@ -108,11 +144,29 @@ export function obfuscateSong(song: Song): Song {
       lastLayerInTick.set(tick, layerIdToAddNoteTo + 1);
 
       const layerToAddNoteTo =
-        output.layers[layerIdToAddNoteTo] || output.createLayer();
+        this.output.layers[layerIdToAddNoteTo] || output.createLayer();
 
-      output.setNote(tick, layerToAddNoteTo, newNote);
+      this.output.setNote(tick, layerToAddNoteTo, note);
+    };
+
+    for (const layer of this.song.layers) {
+      // Skip locked and silent layers
+      if (layer.isLocked || layer.volume === 0) {
+        continue;
+      }
+
+      for (const tickStr in layer.notes) {
+        const note = layer.notes[tickStr];
+        const tick = parseInt(tickStr);
+
+        // Skip silent notes except if they are tempo changers
+        const isTempoChanger = tempoChangerIds.includes(note.instrument); // TODO: doesn't work yet
+        if (note.velocity === 0 && !isTempoChanger) continue;
+
+        // Add obfuscated note to output
+        const newNote = getObfuscatedNote(note, layer);
+        addNoteToOutput(tick, newNote);
+      }
     }
   }
-
-  return output;
 }
