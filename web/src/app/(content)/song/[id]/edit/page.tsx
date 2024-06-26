efde4113921b3b26f7f168a841b0663a91ab6bc5
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-06-25 23:53:22 -0300

    fix: edit song page not kicking user when not logged in
    Co-Authored-By: Nicolas Vycas Nery <vycasnicolas@gmail.com>

--- web/src/app/(content)/song/[id]/edit/page.tsx
+++ web/src/app/(content)/song/[id]/edit/page.tsx
@@ -1,6 +1,12 @@
+import { redirect } from 'next/navigation';
+
+import { checkLogin } from '@web/src/modules/auth/features/auth.utils';
 import { EditSongPage } from '@web/src/modules/song-edit/components/client/EditSongPage';
 
-function Page({ params }: { params: { id: string } }) {
+async function Page({ params }: { params: { id: string } }) {
+  const isLogged = await checkLogin();
+  if (!isLogged) redirect('/login?redirect=/upload');
+
   const { id } = params;
 
   return <EditSongPage id={id} />;
