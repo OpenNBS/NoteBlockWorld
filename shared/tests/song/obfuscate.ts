e04843081738db0653614927a9cb972f259905ee
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-06-27 04:28:04 -0300

    chore: add list of a couple things to test later

---
+++ shared/tests/song/obfuscate.ts
@@ -0,0 +1,17 @@
+// TODO: write tests
+
+// test that there are no gaps in each column of the song
+
+// test that all tempo changers are kept
+
+// test that there's only one tempo changer instrument and it's the last one
+
+// test that no instruments are unused
+
+// test that all instruments are named an empty string or 'Tempo Changer'
+
+// test that all layers have all properties set to the default values (name empty, unlocked, volume 100, panning 0)
+
+// test that no silent notes are present
+
+// test that song stats are cleared
