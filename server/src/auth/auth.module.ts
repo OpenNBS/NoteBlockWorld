88917803071826ec3eb751f721151025f9c75786
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-07-06 01:03:04 -0300

    refactor: remove default value for JWT expiration time
    Co-Authored-By: Nicolas Vycas Nery <vycasnicolas@gmail.com>

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -20,17 +20,15 @@ import { JwtStrategy } from './strategies/JWT.strategy';
       useFactory: async (config: ConfigService) => {
         const JWT_SECRET = config.get('JWT_SECRET');
         const JWT_EXPIRES_IN = config.get('JWT_EXPIRES_IN');
+
         if (!JWT_SECRET) {
           Logger.error('JWT_SECRET is not set');
           throw new Error('JWT_SECRET is not set');
         }
-        if (!JWT_EXPIRES_IN) {
-          Logger.warn('JWT_EXPIRES_IN is not set, using default of 60s');
-        }
 
         return {
           secret: JWT_SECRET,
-          signOptions: { expiresIn: JWT_EXPIRES_IN || '60s' },
+          signOptions: { expiresIn: JWT_EXPIRES_IN },
         };
       },
     }),
