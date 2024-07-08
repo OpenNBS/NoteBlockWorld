7267f4e43f3c953e66ddd6b955525b22b6529869
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-07-07 22:14:58 -0300

    fix: font not rendering on backend (replace `node-canvas` with `@napi-rs/canvas`)
    Fixes font not rendering on backend due to a bug in node-canvas v2.11.2.
    See: https://github.com/Automattic/node-canvas/issues/2285
    
    Couldn't downgrade to v2.10.2 due to build issues on Windows.
    See: https://github.com/Automattic/node-canvas/issues/1773#issuecomment-832364994
    
    R
    
    v3.0.0 of node-canvas will remove node-pre-gyp which should make it a bit easier to install?
    See: https://github.com/Automattic/node-canvas/releases/tag/v3.0.0-rc2

--- web/next.config.js
+++ web/next.config.js
@@ -3,7 +3,10 @@
 const nextConfig = {
   // See: https://github.com/Automattic/node-canvas/issues/867#issuecomment-1925284985
   webpack: (config) => {
-    config.externals.push({ canvas: 'commonjs canvas' });
+    config.externals.push({
+      '@napi-rs/canvas': 'commonjs @napi-rs/canvas',
+    });
+
     return config;
   },
   images: {
