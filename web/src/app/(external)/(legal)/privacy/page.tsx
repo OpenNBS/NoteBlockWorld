2ec69451d23c7e61c672a70df7a6205b488fada4
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-07-10 15:58:24 -0300

    refactor: move document sources to docs subfolder

--- web/src/app/(external)/(legal)/privacy/page.tsx
+++ web/src/app/(external)/(legal)/privacy/page.tsx
@@ -4,7 +4,7 @@ import path from 'path';
 import { CustomMarkdown } from '@web/src/modules/shared/components/CustomMarkdown';
 
 async function PrivacyPolicyPage() {
-  const fullPath = path.join('./public/privacy.md');
+  const fullPath = path.join('./public/docs/privacy.md');
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
   return <CustomMarkdown MarkdownContent={fileContents} />;
--- web/src/app/(external)/(legal)/terms/page.tsx
+++ web/src/app/(external)/(legal)/terms/page.tsx
@@ -4,7 +4,7 @@ import path from 'path';
 import { CustomMarkdown } from '@web/src/modules/shared/components/CustomMarkdown';
 
 async function TermsOfServicePage() {
-  const fullPath = path.join('./public/terms.md');
+  const fullPath = path.join('./public/docs/terms.md');
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
   return <CustomMarkdown MarkdownContent={fileContents} />;
