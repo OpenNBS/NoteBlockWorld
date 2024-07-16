18980a418c50fb52c1d3fd7458af542160bd1e4e
Bernardo Costa <bemcdc@gmail.com>
2024-07-16 02:47:15 -0300

    fix: improve cookie banner design

--- web/src/modules/shared/components/client/CookieBanner.tsx
+++ web/src/modules/shared/components/client/CookieBanner.tsx
@@ -1,33 +1,49 @@
-import { faCookieBite } from '@fortawesome/free-solid-svg-icons';
+import { faClose, faCookieBite } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 
 export const CookieBanner = () => {
   return (
-    <div className='fixed bottom-0 w-full bg-zinc-950/90 border-zinc-700 backdrop-blur-md p-4'>
-      <FontAwesomeIcon icon={faCookieBite} className='text-2xl text-blue-500' />
-      <p className='text-sm text-zinc-400'>
-        We use cookies to enhance your experience in accordance with our{' '}
-        <Link
-          href='/privacy'
-          target='_blank'
-          className='text-blue-500 hover:underline'
-        >
-          privacy policy
-        </Link>
-        . By continuing to browse this site, you agree with our use of cookies.
-        <Link
-          href='https://cookiesandyou.com/'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='text-blue-400 hover:text-blue-300 hover:underline'
-        >
-          What are cookies?
-        </Link>
-      </p>
-      <button className='text-sm text-blue-500 px-3 py-1.5 rounded-lg hover:underline'>
-        Got it!
-      </button>
+    <div className='fixed bottom-0 w-full h-32 z-10 flex justify-center bg-blue-900/80 border-t-blue-500/80 border-t-4 border-zinc-700 backdrop-blur-md p-4'>
+      <div className='w-screen-xl'>
+        {/* Close button */}
+        <button className='absolute top-2 right-2 text-lg text-zinc-400'>
+          <FontAwesomeIcon icon={faClose} />
+        </button>
+        <FontAwesomeIcon
+          icon={faCookieBite}
+          className='text-2xl text-blue-500 w-6 h-6'
+        />
+        <p className='text-sm text-zinc-300'>
+          Hey, fellow note blocker! We use cookies to enhance your experience in
+          accordance with our{' '}
+          <Link
+            href='/privacy'
+            target='_blank'
+            className='text-blue-400 hover:text-blue-300 hover:underline'
+          >
+            privacy policy
+          </Link>
+          . By continuing to browse this site, you agree with our use of
+          cookies.{' '}
+          <Link
+            href='https://cookiesandyou.com/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-blue-400 hover:text-blue-300 hover:underline'
+          >
+            Learn more
+          </Link>
+        </p>
+        <p className='text-xs text-zinc-400'>
+          No, don&apos;t eat those! They don&apos;t taste good.
+        </p>
+        <div className='flex justify-end'>
+          <button className='justify-self-end text-sm bg-blue-600 hover:bg-blue-500 px-3 py-1.5 rounded-lg'>
+            Got it!
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
