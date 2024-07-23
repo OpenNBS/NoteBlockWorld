1b4e945f438c29d799d6811eeca158d3854d13ea
Bernardo Costa <bemcdc@gmail.com>
2024-07-22 21:29:57 -0300

    fix: newline characters being stripped from song description

--- server/src/song/song.util.ts
+++ server/src/song/song.util.ts
@@ -1,7 +1,10 @@
 import { customAlphabet } from 'nanoid';
 
 export function removeExtraSpaces(input: string): string {
-  return input.replace(/\s+/g, ' ').trim();
+  return input
+    .replace(/ +/g, ' ') // replace multiple spaces with one space
+    .replace(/\n\n+/g, '\n\n') // replace 3+ newlines with two newlines
+    .trim(); // remove leading and trailing spaces
 }
 
 const alphabet =
