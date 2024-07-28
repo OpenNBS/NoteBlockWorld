638b75ecf1e162b531f42be4343963ca494b89f9
Bernardo Costa <bemcdc@gmail.com>
2024-07-28 01:14:30 -0300

    fix: truncate instead of round formatted minutes and seconds

--- web/src/modules/shared/util/format.ts
+++ web/src/modules/shared/util/format.ts
@@ -2,8 +2,8 @@ export const formatDuration = (totalSeconds: number) => {
   const minutes = Math.ceil(totalSeconds) / 60;
   const seconds = Math.ceil(totalSeconds) % 60;
 
-  const formattedTime = `${minutes.toFixed().padStart(1, '0')}:${seconds
-    .toFixed()
+  const formattedTime = `${minutes.toString().padStart(1, '0')}:${seconds
+    .toString()
     .padStart(2, '0')}`;
 
   return formattedTime;
