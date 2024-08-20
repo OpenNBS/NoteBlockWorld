653d77f8aeb03b95462569338a5775cc5c8d7edc
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-08-20 02:08:30 -0300

    fix: songs uploaded between 28-29 days ago showing "0 months ago"

--- web/src/modules/shared/util/format.ts
+++ web/src/modules/shared/util/format.ts
@@ -31,7 +31,7 @@ export const formatTimeAgo = (date: Date) => {
 
   const hours = Math.floor(minutes / 60);
 
-  if (hours == 1) {
+  if (hours <= 1) {
     return 'an hour ago';
   }
 
@@ -41,7 +41,7 @@ export const formatTimeAgo = (date: Date) => {
 
   const days = Math.floor(hours / 24);
 
-  if (days == 1) {
+  if (days <= 1) {
     return 'yesterday';
   }
 
@@ -51,7 +51,7 @@ export const formatTimeAgo = (date: Date) => {
 
   const weeks = Math.floor(days / 7);
 
-  if (weeks == 1) {
+  if (weeks <= 1) {
     return 'a week ago';
   }
 
@@ -61,7 +61,7 @@ export const formatTimeAgo = (date: Date) => {
 
   const months = Math.floor(days / 30);
 
-  if (months == 1) {
+  if (months <= 1) {
     return 'a month ago';
   }
 
@@ -71,7 +71,7 @@ export const formatTimeAgo = (date: Date) => {
 
   const years = Math.floor(days / 365);
 
-  if (years == 1) {
+  if (years <= 1) {
     return 'a year ago';
   }
 
