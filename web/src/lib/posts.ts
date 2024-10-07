5b94f60497b0b946db57b1adf0b21768744c24fa
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-10-07 04:40:45 -0300

    fix: remove sorting prefix in post filename when locating via path param

--- web/src/lib/posts.ts
+++ web/src/lib/posts.ts
@@ -14,6 +14,24 @@ export type PostType = {
   authorImage?: string;
 };
 
+const blogPostIds = fs
+  .readdirSync(path.join(process.cwd(), 'posts', 'blog'))
+  .reduce((acc, fileName) => {
+    // Remove ".md" and date prefix to get post ID
+    const postId = fileName.replace(/\.md$/, '').split('_')[1];
+    acc[postId] = fileName;
+    return acc;
+  }, {} as Record<string, string>);
+
+const helpPostIds = fs
+  .readdirSync(path.join(process.cwd(), 'posts', 'help'))
+  .reduce((acc, fileName) => {
+    // Remove ".md" and number prefix to get help article ID
+    const helpId = fileName.replace(/\.md$/, '').split('_')[1];
+    acc[helpId] = fileName;
+    return acc;
+  }, {} as Record<string, string>);
+
 export function getSortedPostsData(
   postsPath: 'help' | 'blog',
   sortBy: 'id' | 'date',
@@ -24,8 +42,8 @@ export function getSortedPostsData(
   const fileNames = fs.readdirSync(postsDirectory);
 
   const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get post ID
-    const id = fileName.replace(/\.md$/, '');
+    // Remove ".md" and prefix from file name to get post ID
+    const id = fileName.replace(/\.md$/, '').split('_')[1];
     return getPostData(postsPath, id);
   });
 
@@ -53,9 +71,21 @@ export function getSortedPostsData(
   return allPostsData.sort((a, b) => sortFunction(a, b));
 }
 
-export function getPostData(postsPath: 'help' | 'blog', postId: string) {
+export function getPostData(
+  postsPath: 'help' | 'blog',
+  postId: string,
+): PostType {
+  // Look for the file in the posts directory that contains postId as suffix
+  const fileName =
+    postsPath === 'blog' ? blogPostIds[postId] : helpPostIds[postId];
+
+  if (!fileName) {
+    throw new Error(`No file found for post ID: ${postId}`);
+  }
+
+  const fullPath = path.join(process.cwd(), 'posts', postsPath, fileName);
+
   // Read markdown file as string
-  const fullPath = path.join(process.cwd(), 'posts', postsPath, `${postId}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
   // Use gray-matter to parse the post metadata section
