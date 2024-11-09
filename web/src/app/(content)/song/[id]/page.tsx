206bb83f9cc87111d00d92bed1c0365c9a6ffd09
encode42 <me@encode42.dev>
2024-11-09 02:27:44 -0500

    Fetch song information and return metadata for song page embeds

--- web/src/app/(content)/song/[id]/page.tsx
+++ web/src/app/(content)/song/[id]/page.tsx
@@ -1,6 +1,39 @@
+import { SongViewDtoType } from '@shared/validation/song/dto/types';
+import type { Metadata } from 'next';
+
+import axios from '@web/src/lib/axios';
 import { SongPage } from '@web/src/modules/song/components/SongPage';
 
-function Page({ params }: { params: { id: string } }) {
+interface SongPage {
+  params: {
+    id: string;
+  };
+}
+
+export async function generateMetadata({
+  params,
+}: SongPage): Promise<Metadata> {
+  let song;
+
+  try {
+    const response = await axios.get<SongViewDtoType>(`/song/${params.id}`);
+    song = response.data;
+  } catch {
+    return {
+      title: 'Unknown song!',
+    };
+  }
+
+  return {
+    title: song.title,
+    description: song.description,
+    openGraph: {
+      images: [song.thumbnailUrl],
+    },
+  };
+}
+
+function Page({ params }: SongPage) {
   const { id } = params;
 
   return <SongPage id={id} />;
