be1dcd96c5b3fce8be7ad437b93cba87c7de610c
tomast1337 <vycasnicolas@gmail.com>
2024-11-12 14:31:19 -0300

    fix: add module name mapping for server imports in jest configuration

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,20 +1,21 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: '.',
-  testRegex: '.*\\.spec\\.ts$',
-  transform: {
-    '^.+\\.(t|j)s$': [
-      'ts-jest',
-      {
-        tsconfig: '<rootDir>/tsconfig.json',
-        ignoreCodes: ['TS151001'],
-      },
-    ],
-  },
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: './coverage',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@shared/(.*)$': '<rootDir>/../shared/$1',
-  },
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: '.',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+        '^.+\\.(t|j)s$': [
+            'ts-jest',
+            {
+                tsconfig: '<rootDir>/tsconfig.json',
+                ignoreCodes: ['TS151001'],
+            },
+        ],
+    },
+    collectCoverageFrom: ['**/*.(t|j)s'],
+    coverageDirectory: './coverage',
+    testEnvironment: 'node',
+    moduleNameMapper: {
+        '^@shared/(.*)$': '<rootDir>/../shared/$1',
+        '^@server/(.*)$': '<rootDir>/src/$1',
+    },
 };
