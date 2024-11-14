1dd4c8799e869b3729961f760c83f0d1ec9a571f
tomast1337 <vycasnicolas@gmail.com>
2024-11-14 17:01:13 -0300

    test: enhance SongUploadService unit tests with mock FileService and UserService

--- server/src/song/song-upload/song-upload.service.spec.ts
+++ server/src/song/song-upload/song-upload.service.spec.ts
@@ -5,19 +5,40 @@ import { UserService } from '@server/user/user.service';
 
 import { SongUploadService } from './song-upload.service';
 
+const mockFileService = {
+  uploadSong: jest.fn(),
+  uploadPackedSong: jest.fn(),
+  uploadThumbnail: jest.fn(),
+  getSongFile: jest.fn(),
+};
+
+const mockUserService = {
+  findByID: jest.fn(),
+};
+
 describe('SongUploadService', () => {
   let service: SongUploadService;
+  let _fileService: FileService;
+  let _userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         SongUploadService,
-        { provide: FileService, useValue: {} },
-        { provide: UserService, useValue: {} },
+        {
+          provide: FileService,
+          useValue: mockFileService,
+        },
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
       ],
     }).compile();
 
     service = module.get<SongUploadService>(SongUploadService);
+    _fileService = module.get<FileService>(FileService);
+    _userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
