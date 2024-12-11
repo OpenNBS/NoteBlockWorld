699accc5a73e6145c5c222b25155721ba69e1086
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-12-11 04:45:13 -0300

    fix: ad slot on song page

--- web/src/modules/song/components/SongPage.tsx
+++ web/src/modules/song/components/SongPage.tsx
@@ -13,6 +13,7 @@ import {
 } from './SongPageButtons';
 import { ErrorBox } from '../../shared/components/client/ErrorBox';
 import { formatTimeAgo } from '../../shared/util/format';
+import { InterSectionAdSlot } from '../../shared/components/client/ads/AdSlots';
 
 export async function SongPage({ id }: { id: string }) {
   let song;
@@ -25,52 +26,55 @@ export async function SongPage({ id }: { id: string }) {
   }
 
   return (
-    <div className='grid grid-cols-8 gap-12'>
-      <div className='col-span-full lg:col-span-5 flex flex-col gap-4'>
-        {/* Song thumbnail */}
-        {/* TODO: implement loading https://github.com/vercel/next.js/discussions/50617 */}
-        <picture className='bg-zinc-800 aspect-[5/3] rounded-xl'>
-          <Image
-            width={1280}
-            height={720}
-            alt='Song thumbnail'
-            src={song.thumbnailUrl}
-            className='w-full h-full rounded-xl'
-          />
-        </picture>
+    <>
+      <div className='grid grid-cols-8 gap-12'>
+        <div className='col-span-full lg:col-span-5 flex flex-col gap-4'>
+          {/* Song thumbnail */}
+          {/* TODO: implement loading https://github.com/vercel/next.js/discussions/50617 */}
+          <picture className='bg-zinc-800 aspect-[5/3] rounded-xl'>
+            <Image
+              width={1280}
+              height={720}
+              alt='Song thumbnail'
+              src={song.thumbnailUrl}
+              className='w-full h-full rounded-xl'
+            />
+          </picture>
 
-        <h1 className='text-xl font-bold'>{song.title}</h1>
+          <h1 className='text-xl font-bold'>{song.title}</h1>
 
-        {/* Uploader and actions */}
-        <div className='flex flex-row flex-wrap justify-start items-center gap-8 w-full'>
-          <UploaderBadge user={song.uploader} />
-          {/* <FollowButton /> */}
-          <div className='flex-grow'></div>
-          <div className='flex flex-row gap-4'>
-            {/* <LikeButton /> */}
-            <ShareButton songId={song.publicId} />
-            <OpenSongInNBSButton song={song} />
-            <DownloadSongButton song={song} />
+          {/* Uploader and actions */}
+          <div className='flex flex-row flex-wrap justify-start items-center gap-8 w-full'>
+            <UploaderBadge user={song.uploader} />
+            {/* <FollowButton /> */}
+            <div className='flex-grow'></div>
+            <div className='flex flex-row gap-4'>
+              {/* <LikeButton /> */}
+              <ShareButton songId={song.publicId} />
+              <OpenSongInNBSButton song={song} />
+              <DownloadSongButton song={song} />
+            </div>
+          </div>
+
+          {/* Views, upload date, description */}
+          <div className='flex flex-col p-3 gap-2 bg-zinc-800 rounded-xl'>
+            <div className='text-sm text-zinc-300'>
+              {song.playCount} {song.playCount == 1 ? 'view' : 'views'} •{' '}
+              {formatTimeAgo(new Date(song.createdAt))}
+            </div>
+            <p className='leading-tight whitespace-pre-line'>
+              {song.description}
+            </p>
+            <LicenseInfo license={song.license} />
           </div>
         </div>
 
-        {/* Views, upload date, description */}
-        <div className='flex flex-col p-3 gap-2 bg-zinc-800 rounded-xl'>
-          <div className='text-sm text-zinc-300'>
-            {song.playCount} {song.playCount == 1 ? 'view' : 'views'} •{' '}
-            {formatTimeAgo(new Date(song.createdAt))}
-          </div>
-          <p className='leading-tight whitespace-pre-line'>
-            {song.description}
-          </p>
-          <LicenseInfo license={song.license} />
+        {/* Right panel - song details */}
+        <div className='col-span-full lg:col-span-3'>
+          <SongDetails song={song} />
         </div>
       </div>
-
-      {/* Right panel - song details */}
-      <div className='col-span-full lg:col-span-3'>
-        <SongDetails song={song} />
-      </div>
-    </div>
+      <InterSectionAdSlot />
+    </>
   );
 }
