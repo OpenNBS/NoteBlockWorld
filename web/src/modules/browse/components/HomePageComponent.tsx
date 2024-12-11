5ab18b6cc57e15047792ee09fd0bbc163738b577
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-12-11 04:32:13 -0300

    fix: remove max-height attribute (google doesn't respect it anyway)

--- web/src/modules/browse/components/HomePageComponent.tsx
+++ web/src/modules/browse/components/HomePageComponent.tsx
@@ -64,7 +64,7 @@ export const HomePageComponent = () => {
         </>
       )}
 
-      <div className='rounded-xl bg-zinc-800/50 p-2 my-8 h-32 lg:max-h-48 w-full min-w-64 text-sm text-zinc-400'>
+      <div className='rounded-xl bg-zinc-800/50 p-2 my-8 h-32 w-full min-w-64 text-sm text-zinc-400'>
         <InterSectionAdSlot />
       </div>
 
