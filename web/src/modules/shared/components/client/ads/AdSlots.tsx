67b7ab94c28ab76214f4dbf4002a3619c63802dd
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-12-11 03:11:10 -0300

    fix: optimize unnecessary script tags for ad placement

--- web/src/modules/shared/components/client/ads/AdSlots.tsx
+++ web/src/modules/shared/components/client/ads/AdSlots.tsx
@@ -1,35 +1,28 @@
-import Script from 'next/script';
+import { useEffect } from 'react';
 
 import useAdSenseClient from './useAdSenseClient';
 
 export const InterSectionAdSlot = () => {
   const pubId = useAdSenseClient();
 
+  useEffect(() => {
+    if (window) {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
+  }, []);
+
   if (!pubId) {
     return 'AdSense Client ID is not set';
   }
 
   return (
-    <>
-      <Script
-        async
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${pubId}`}
-        crossOrigin='anonymous'
-      />
-      <ins
-        className='adsbygoogle'
-        style={{ display: 'block' }}
-        data-ad-client={pubId}
-        data-ad-slot='4046918224'
-        data-ad-format='auto'
-        data-full-width-responsive='true'
-      ></ins>
-      <Script
-        id='adsbygoogle-push'
-        dangerouslySetInnerHTML={{
-          __html: `(adsbygoogle = window.adsbygoogle || []).push({});`,
-        }}
-      />
-    </>
+    <ins
+      className='adsbygoogle'
+      style={{ display: 'block' }}
+      data-ad-client={pubId}
+      data-ad-slot='4995642586'
+      data-ad-format='auto'
+      data-full-width-responsive='true'
+    ></ins>
   );
 };
