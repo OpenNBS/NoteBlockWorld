17fbdb60100d97445e7bb465a161b4548341f09c
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-12-11 05:42:52 -0300

    feat: add side rail ad slots

--- web/src/modules/shared/components/client/ads/AdSlots.tsx
+++ web/src/modules/shared/components/client/ads/AdSlots.tsx
@@ -36,3 +36,28 @@ export const InterSectionAdSlot = () => {
     </div>
   );
 };
+
+export const SideRailAdSlot = () => {
+  const pubId = useAdSenseClient();
+
+  useEffect(() => {
+    if (window) {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
+  }, []);
+
+  if (!pubId) {
+    return 'AdSense Client ID is not set';
+  }
+
+  return (
+    <ins
+      className='adsbygoogle'
+      style={{ display: 'block' }}
+      data-ad-client={pubId}
+      data-ad-slot='4046918224'
+      data-ad-format='auto'
+      data-full-width-responsive='true'
+    ></ins>
+  );
+};
--- web/src/modules/shared/components/layout/NavbarLayout.tsx
+++ web/src/modules/shared/components/layout/NavbarLayout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { CookieBanner } from '../client/CookieBanner';
+import { SideRailAdSlot } from '../client/ads/AdSlots';
 
 type TNavbarLayoutProps = {
   children: React.ReactNode;
@@ -8,17 +9,25 @@ type TNavbarLayoutProps = {
 
 async function Layout({ children }: TNavbarLayoutProps) {
   return (
-    <div className='w-full h-full min-h-screen flex flex-col justify-between items-center bg-zinc-900'>
-      {/* Header */}
-      <Header />
-      {/* Main content */}
-      <main className='w-full max-w-screen-xl flex-grow pt-24 px-6 sm:px-10 pb-10'>
-        {children}
-      </main>
-      {/* Footer */}
-      <Footer />
-      <CookieBanner />
-    </div>
+    <>
+      <div className='w-full h-full min-h-screen flex flex-col justify-between items-center bg-zinc-900'>
+        {/* Header */}
+        <Header />
+        {/* Main content */}
+        <div className='fixed left-0 mx-[2%] top-24 h-[calc(100vh-12rem)] hidden xl:block w-36 bg-zinc-800/50 rounded-xl'>
+          <SideRailAdSlot />
+        </div>
+        <main className='w-full 2xl:max-w-screen-xl max-w-screen-lg pt-24 2xl:px-16 lg:px-28 px-6 sm:px-10 pb-10'>
+          {children}
+        </main>
+        <div className='fixed right-0 mx-[2%] top-24 h-[calc(100vh-12rem)] hidden xl:block w-36 bg-zinc-800/50 rounded-xl'>
+          <SideRailAdSlot />
+        </div>
+        {/* Footer */}
+        <Footer />
+        <CookieBanner />
+      </div>
+    </>
   );
 }
 
