a3552c571eccadc37bb10b27a4b0108bafcfb0a0
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-12-11 04:03:55 -0300

    fix: remove flex and set intrinsic size to ad container

--- web/src/modules/browse/components/HomePageComponent.tsx
+++ web/src/modules/browse/components/HomePageComponent.tsx
@@ -64,7 +64,7 @@ export const HomePageComponent = () => {
         </>
       )}
 
-      <div className='rounded-xl bg-zinc-800/50 p-2 my-8 h-32 text-sm text-zinc-400 flex items-center justify-center'>
+      <div className='rounded-xl bg-zinc-800/50 p-2 my-8 h-32 w-full min-w-96 text-sm text-zinc-400'>
         <InterSectionAdSlot />
       </div>
 
