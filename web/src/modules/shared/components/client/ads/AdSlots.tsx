700b4788f1de38cecfdd111bbb0e86992d2dce2f
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-12-12 04:17:59 -0300

    fix: attempt to make side ad slot container fit height to ad content

--- web/src/modules/shared/components/client/ads/AdSlots.tsx
+++ web/src/modules/shared/components/client/ads/AdSlots.tsx
@@ -107,7 +107,7 @@ export const SideRailAdSlot = ({ className }: { className?: string }) => {
   return (
     <AdTemplate
       className={cn(
-        'flex-0 sticky mb-8 top-24 max-h-[calc(100vh-9rem)] hidden xl:block w-36 bg-zinc-800/50 rounded-xl',
+        'flex-0 flex-grow-0 sticky mb-8 top-24 max-h-[calc(100vh-9rem)] hidden xl:block w-36 bg-zinc-800/50 rounded-xl',
         className,
       )}
       adSlot='4995642586'
