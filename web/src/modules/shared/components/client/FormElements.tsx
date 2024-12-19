b59259d9f6bcd1339f8885a82aab48386522a041
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-12-18 22:34:31 -0300

    fix: show outline on select element with error even when focused

--- web/src/modules/shared/components/client/FormElements.tsx
+++ web/src/modules/shared/components/client/FormElements.tsx
@@ -177,7 +177,9 @@ export const Select = forwardRef<
           {...rest}
           className={cn(
             `block h-12 w-full rounded-lg bg-transparent border-2 ${
-              errorMessage ? 'border-red-500' : 'border-zinc-500'
+              errorMessage
+                ? 'border-red-500 focus:outline-red-500'
+                : 'border-zinc-500'
             }  disabled:border-zinc-700 disabled:cursor-not-allowed disabled:text-zinc-500 p-2`,
             props.className,
           )}
