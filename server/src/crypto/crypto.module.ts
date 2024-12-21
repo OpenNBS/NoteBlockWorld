f5442adc3c4975e58a93f2a5026321c53934a1a2
tomast1337 <vycasnicolas@gmail.com>
2024-12-21 11:18:15 -0300

    refactor: convert CryptoModule to use @Module decorator for improved structure

--- server/src/crypto/crypto.module.ts
+++ server/src/crypto/crypto.module.ts
@@ -1,22 +1,18 @@
 import { DynamicModule, Module } from '@nestjs/common';
 
-import { CryptoService } from './crypto.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CryptoService } from './crypto.service';
 
-export class CryptoModule {
-  static forRoot(): DynamicModule {
-    return {
-      module: CryptoModule,
-      imports: [ConfigModule.forRoot()],
-      providers: [
-        {
-          provide: 'SALTS_ROUNDS',
-          useValue: (configService: ConfigService) =>
-            configService.getOrThrow<string>('SALTS_ROUNDS'),
-        },
-        CryptoService,
-      ],
-      exports: [CryptoService],
-    };
-  }
-}
+@Module({
+  imports: [ConfigModule.forRoot()],
+  providers: [
+    {
+      provide: 'SALTS_ROUNDS',
+      useValue: (configService: ConfigService) =>
+        configService.getOrThrow<string>('SALTS_ROUNDS'),
+    },
+    CryptoService,
+  ],
+  exports: [CryptoService],
+})
+export class CryptoModule {}
