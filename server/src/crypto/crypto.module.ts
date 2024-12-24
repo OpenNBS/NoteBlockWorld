aa816e64024a266a52148da387a43afc27531633
tomast1337 <vycasnicolas@gmail.com>
2024-12-23 21:07:30 -0300

    refactor: update CryptoModule to use useFactory for SALTS_ROUNDS and remove forRoot method

--- server/src/crypto/crypto.module.ts
+++ server/src/crypto/crypto.module.ts
@@ -6,13 +6,34 @@ import { CryptoService } from './crypto.service';
 @Module({
   imports: [ConfigModule.forRoot()],
   providers: [
+    ConfigService,
     {
       provide: 'SALTS_ROUNDS',
-      useValue: (configService: ConfigService) =>
-        configService.getOrThrow<string>('SALTS_ROUNDS'),
+      useFactory: (configService: ConfigService) =>
+        configService.getOrThrow<number>('SALTS_ROUNDS'),
+      inject: [ConfigService],
     },
     CryptoService,
   ],
   exports: [CryptoService],
 })
-export class CryptoModule {}
+export class CryptoModule {
+  /*
+  static forRoot(): DynamicModule {
+    return {
+      module: CryptoModule,
+      imports: [ConfigModule.forRoot()],
+      providers: [
+        ConfigService,
+        {
+          provide: 'SALTS_ROUNDS',
+          useValue: (configService: ConfigService) =>
+            configService.getOrThrow<number>('SALTS_ROUNDS'),
+        },
+        CryptoService,
+      ],
+      exports: [CryptoService],
+    };
+  }
+ */
+}
