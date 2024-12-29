313b910d3e73a06a0194f19eb26750e49dc710b3
tomast1337 <vycasnicolas@gmail.com>
2024-12-29 14:41:40 -0300

    feat: implement Mailing module with controller and service, including unit tests

---
+++ server/src/mailing/mailing.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { MailingService } from './mailing.service';
+
+@Controller()
+export class MailingController {
+  constructor(private readonly mailingService: MailingService) {}
+}
