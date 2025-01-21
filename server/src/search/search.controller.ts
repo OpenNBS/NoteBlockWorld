30c1d241e11c236067a24d8d22379dd9b49069d2
tomast1337 <vycasnicolas@gmail.com>
2025-01-20 22:23:47 -0300

    feat: update search controller to use SearchQueryDTO for query parameters

--- server/src/search/search.controller.ts
+++ server/src/search/search.controller.ts
@@ -1,14 +1,16 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { PageQueryDTO } from '@shared/validation/common/dto/PageQuery.dto';
 
+import { ApiTags } from '@nestjs/swagger';
+import { SearchQueryDTO } from '@shared/validation/common/dto/SearchQuery.dto';
 import { SearchService } from './search.service';
 
 @Controller('search')
+@ApiTags('search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
 
   @Get()
-  async search(@Query() query: PageQueryDTO) {
+  async search(@Query() query: SearchQueryDTO) {
     return await this.searchService.search(query);
   }
 }
