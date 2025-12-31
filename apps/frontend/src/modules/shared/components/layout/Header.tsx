03fc03636fb5d332adfd3210f0a065b6607da8db
Bentroen <29354120+Bentroen@users.noreply.github.com>
2025-12-31 11:49:11 -0300

    fix: make navbar scrollable to mitigate partial responsiveness

--- apps/frontend/src/modules/shared/components/layout/Header.tsx
+++ apps/frontend/src/modules/shared/components/layout/Header.tsx
@@ -33,7 +33,7 @@ export async function Header() {
   return (
     <header className='fixed w-[calc(100vw_-_16px)] h-14 flex flex-row justify-center items-center bg-zinc-900 border-b border-zinc-700 py-2 z-10'>
       {/* Navbar */}
-      <nav className='w-full flex flex-row justify-between items-center gap-8 md:gap-12 max-w-screen-xl px-6 sm:px-10'>
+      <nav className='w-full flex flex-row justify-between items-center gap-8 md:gap-12 max-w-screen-xl px-6 sm:px-10 overflow-x-auto box-content [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden'>
         {/* Icon */}
         <div className='flex-0 flex justify-start lg:justify-center min-w-fit'>
           <Link href='/'>
